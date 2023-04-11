acdad58e6a5a73b8ad3f03fb8376f28f3a491ec5
Eduard <edpufa@inspalamos.cat>
2023-04-11 19:55:43 +0200

    classe Idioma

---
+++ Koobing_Admin/src/main/java/edu/pujadas/koobing_admin/Models/Idioma.java
@@ -0,0 +1,33 @@
+package edu.pujadas.koobing_admin.Models;
+
+public class Idioma
+{
+    int idIdioma;
+    String nomIdioma;
+
+    public Idioma(int idIdioma, String nomIdioma)
+    {
+        this.idIdioma = idIdioma;
+        this.nomIdioma = nomIdioma;
+    }
+
+    public int getIdIdioma()
+    {
+        return idIdioma;
+    }
+
+    public void setIdIdioma(int idIdioma)
+    {
+        this.idIdioma = idIdioma;
+    }
+
+    public String getNomIdioma()
+    {
+        return nomIdioma;
+    }
+
+    public void setNomIdioma(String nomIdioma)
+    {
+        this.nomIdioma = nomIdioma;
+    }
+}
