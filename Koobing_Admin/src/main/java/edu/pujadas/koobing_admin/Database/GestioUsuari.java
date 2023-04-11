0cccaed2f354e01c6eeaeb84b3db7e15accd4545
Eduard <edpufa@inspalamos.cat>
2023-04-11 20:20:04 +0200

    block try - catch

--- Koobing_Admin/src/main/java/edu/pujadas/koobing_admin/Database/GestioUsuari.java
+++ Koobing_Admin/src/main/java/edu/pujadas/koobing_admin/Database/GestioUsuari.java
@@ -1,5 +1,7 @@
 package edu.pujadas.koobing_admin.Database;
 
+import edu.pujadas.koobing_admin.Models.Usuari;
+
 import java.sql.Statement;
 
 public class GestioUsuari implements OperacionCRUD
@@ -9,23 +11,57 @@ public class GestioUsuari implements OperacionCRUD
     public void crear()
     {
         // todo insertar usuari
+        try
+        {
+
+        }
+        catch (Exception e)
+        {
+            System.out.println("El usuari no se ha pogut insertar ;(");
+        }
     }
 
     @Override
     public void modificar()
     {
         //todo modificar usuari
+        try
+        {
+
+        }
+        catch (Exception e)
+        {
+            System.out.println("Hi ha hagut una error a la modificacio");
+        }
     }
 
     @Override
     public void eliminar()
     {
         //todo eliminar usuari
+
+        try
+        {
+
+        }
+        catch (Exception e)
+        {
+            System.out.println("El usuari no se ha pogut eliminar");
+        }
     }
 
     @Override
     public void consultar()
     {
         //todo consultar usuari
+
+        try
+        {
+
+        }
+        catch (Exception e)
+        {
+            System.out.println("No se han trobat usuaris ;( ");
+        }
     }
 }
