246d54f6ad292254c61741ceedbb7543d1866037
Eduard <edpufa@inspalamos.cat>
2023-04-11 20:14:57 +0200

    Interficia Crud Op
    Aquesta interficie es per poder repetir els metodes

---
+++ Koobing_Admin/src/main/java/edu/pujadas/koobing_admin/Database/OperacionCRUD.java
@@ -0,0 +1,14 @@
+package edu.pujadas.koobing_admin.Database;
+
+import java.sql.Statement;
+
+public interface OperacionCRUD
+{
+
+
+    void crear();
+    void modificar();
+    void eliminar();
+    void consultar();
+
+}
