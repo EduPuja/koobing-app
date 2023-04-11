51a309fc3635fa4484cf9402dcf32fa75cfa1c14
Eduard <edpufa@inspalamos.cat>
2023-04-11 18:34:38 +0200

    Admin

---
+++ Koobing_Admin/src/main/java/module-info.java
@@ -0,0 +1,14 @@
+module edu.pujadas.koobing_admin {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.bootstrapfx.core;
+
+
+    opens edu.pujadas.koobing_admin to javafx.fxml;
+    exports edu.pujadas.koobing_admin;
+}
