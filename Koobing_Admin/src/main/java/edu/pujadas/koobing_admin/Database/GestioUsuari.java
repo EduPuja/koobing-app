4d0f630ce910bca225b397b92c5848e2d60ccd6a
Eduard <edpufa@inspalamos.cat>
2023-04-12 16:21:34 +0200

    crear,consultar,eliminar done
    falta modificar

--- Koobing_Admin/src/main/java/edu/pujadas/koobing_admin/Database/GestioUsuari.java
+++ Koobing_Admin/src/main/java/edu/pujadas/koobing_admin/Database/GestioUsuari.java
@@ -2,18 +2,24 @@ package edu.pujadas.koobing_admin.Database;
 
 import edu.pujadas.koobing_admin.Models.Usuari;
 
+import java.sql.ResultSet;
 import java.sql.Statement;
 
 public class GestioUsuari
 {
 
 
+    /**
+     * Metode que inserta un usuari a la base de dates
+     * @param usuari objecte usuari que és creat a memória
+     */
     public void crearUsuari(Usuari usuari)
     {
         // todo insertar usuari
         try
         {   //connexio usuari
             Statement con = ConnexioMYSQL.connexioMYSQL();
+
 
             String query ="INSERT INTO `usuari`( `avatar`, `nom`, `cognom`, `data_naix`, `email`, `password`) " +
                     "VALUES ('"+usuari.getAvatar()+"','"+usuari.getNom()+"','"+usuari.getCognom()+"','"+usuari.getDataNaix()+"','"+usuari.getEmail()+"','"+usuari.getPassword()+") ";
@@ -38,7 +44,6 @@ public class GestioUsuari
         {
             Statement con = ConnexioMYSQL.connexioMYSQL();
 
-
         }
         catch (Exception e)
         {
@@ -46,14 +51,26 @@ public class GestioUsuari
         }
     }
 
-
-    public void eliminarUsuari()
+    /**
+     * Metode per eliminar un usuari de la base de dades
+     * @param dniUsuari li passes un DNI d'usuari
+     */
+    public void eliminarUsuari(String dniUsuari)
     {
         //todo eliminar usuari
 
         try
+
         {
             Statement con = ConnexioMYSQL.connexioMYSQL();
+            String query = "DELETE FROM `usuari` WHERE `dni` = '" + dniUsuari + "'";
+
+            if(con.executeUpdate(query) == 1)
+            {
+                System.out.println("Usuari eliminat correctament");
+            }
+            else System.out.println("La baixa no s'ha completat ");
+            con.close();
         }
         catch (Exception e)
         {
@@ -62,6 +79,9 @@ public class GestioUsuari
     }
 
 
+    /**
+     * Metode per consultar els usuaris de la base de dades
+     */
     public void consultarUsuari()
     {
         //todo consultar usuari
@@ -69,10 +89,24 @@ public class GestioUsuari
         try
         {
             Statement con = ConnexioMYSQL.connexioMYSQL();
+            String query = "select * from usuari";
+            ResultSet rs = con.executeQuery(query);
+            while (rs.next())
+            {
+                System.out.println("Id_usuari: " +rs.getInt("id_usuari"));
+                System.out.println("DNI: " +rs.getString("dni"));
+                System.out.println("NOM: " +rs.getString("nom"));
+                System.out.println("COGNOM: " +rs.getString("cognom"));
+                System.out.println("Data Naix: " +rs.getDate("data_naix"));
+                System.out.println("Email: " +rs.getString("email"));
+                System.out.println("Password: " +rs.getString("password"));
+            }
+            con.close();
         }
         catch (Exception e)
         {
             System.out.println("No se han trobat usuaris ;( ");
+
         }
     }
 }
