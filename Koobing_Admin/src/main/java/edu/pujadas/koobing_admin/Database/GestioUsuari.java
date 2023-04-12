ed7805c8d6aa43cc4462434c20ccefe7073ff8ed
Eduard <edpufa@inspalamos.cat>
2023-04-12 16:03:13 +0200

    crear usuari

--- Koobing_Admin/src/main/java/edu/pujadas/koobing_admin/Database/GestioUsuari.java
+++ Koobing_Admin/src/main/java/edu/pujadas/koobing_admin/Database/GestioUsuari.java
@@ -4,16 +4,25 @@ import edu.pujadas.koobing_admin.Models.Usuari;
 
 import java.sql.Statement;
 
-public class GestioUsuari implements OperacionCRUD
+public class GestioUsuari
 {
-    Statement con = ConnexioMYSQL.connexioMYSQL();
-    @Override
-    public void crear()
+
+
+    public void crearUsuari(Usuari usuari)
     {
         // todo insertar usuari
         try
-        {
+        {   //connexio usuari
+            Statement con = ConnexioMYSQL.connexioMYSQL();
 
+            String query ="INSERT INTO `usuari`( `avatar`, `nom`, `cognom`, `data_naix`, `email`, `password`) " +
+                    "VALUES ('"+usuari.getAvatar()+"','"+usuari.getNom()+"','"+usuari.getCognom()+"','"+usuari.getDataNaix()+"','"+usuari.getEmail()+"','"+usuari.getPassword()+") ";
+
+            if(con.executeUpdate(query) == 1)
+            {
+                System.out.println("Usuari insertat successfully");
+            }
+            else System.out.println("Usuari not inserted"); con.close();
         }
         catch (Exception e)
         {
@@ -21,12 +30,14 @@ public class GestioUsuari implements OperacionCRUD
         }
     }
 
-    @Override
-    public void modificar()
+
+    public void modificarUsuari()
     {
         //todo modificar usuari
         try
         {
+            Statement con = ConnexioMYSQL.connexioMYSQL();
+
 
         }
         catch (Exception e)
@@ -35,14 +46,14 @@ public class GestioUsuari implements OperacionCRUD
         }
     }
 
-    @Override
-    public void eliminar()
+
+    public void eliminarUsuari()
     {
         //todo eliminar usuari
 
         try
         {
-
+            Statement con = ConnexioMYSQL.connexioMYSQL();
         }
         catch (Exception e)
         {
@@ -50,14 +61,14 @@ public class GestioUsuari implements OperacionCRUD
         }
     }
 
-    @Override
-    public void consultar()
+
+    public void consultarUsuari()
     {
         //todo consultar usuari
 
         try
         {
-
+            Statement con = ConnexioMYSQL.connexioMYSQL();
         }
         catch (Exception e)
         {
