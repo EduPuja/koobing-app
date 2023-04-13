34c8a61aea04e6f4cf9166f60fdd00c047bc9809
Eduard <edpufa@inspalamos.cat>
2023-04-13 15:23:37 +0200

    Afegint dades a la taula

--- Koobing_Admin/src/main/java/edu/pujadas/koobing_admin/Controllers/HomeController.java
+++ Koobing_Admin/src/main/java/edu/pujadas/koobing_admin/Controllers/HomeController.java
@@ -1,7 +1,35 @@
 package edu.pujadas.koobing_admin.Controllers;
 
-public class HomeController
+import javafx.fxml.Initializable;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TableColumn;
+import javafx.scene.image.ImageView;
+import org.controlsfx.control.tableview2.TableView2;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class HomeController implements Initializable
 {
     // todo taules recollir de la base de datos la info del usuari / llibre / autor limit 10
+    public ImageView avatarWorker;
+
+    // ---- Usuari Stuff ----   //
+    public Tab usuarisTab;
+    public TableView2 taulaUsuaris;
+    public TableColumn idUsuari;
+    public TableColumn dniColum;
+    public TableColumn nomColum;
+    public TableColumn cognomColum;
+    public TableColumn dataNaixColum;
+    public TableColumn emailColum;
+    public TableColumn passwordColum;
+    // ---- End usuari Stuff ---- //
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle)
+    {
+
+    }
 
 }
