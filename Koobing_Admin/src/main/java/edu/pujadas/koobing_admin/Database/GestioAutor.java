ed237ca53c838ab91ff64f9968df2fdba1cff203
Eduard <edpufa@inspalamos.cat>
2023-04-13 16:49:33 +0200

    creant gestio autor

---
+++ Koobing_Admin/src/main/java/edu/pujadas/koobing_admin/Database/GestioAutor.java
@@ -0,0 +1,18 @@
+package edu.pujadas.koobing_admin.Database;
+
+import edu.pujadas.koobing_admin.Models.Autor;
+
+public class GestioAutor
+{
+    public void crearAutor(Autor autor)
+    {
+        try
+        {
+
+        }
+        catch (Exception e)
+        {
+            System.out.println("El autor no se ha insertat correctament");
+        }
+    }
+}
