3c03ce412ebe7306220d0f85a2f0020cb71abd4e
EduPC <edpufa@inspalamos.cat>
2023-04-17 23:58:39 +0200

    fent consultarTreballadors
    encara no esta acabat

--- Koobing_Admin/src/main/java/edu/pujadas/koobing_admin/Database/GestioTreballador.java
+++ Koobing_Admin/src/main/java/edu/pujadas/koobing_admin/Database/GestioTreballador.java
@@ -2,6 +2,8 @@ package edu.pujadas.koobing_admin.Database;
 
 import edu.pujadas.koobing_admin.Models.Treballador;
 
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.util.ArrayList;
 
 public class GestioTreballador
@@ -19,11 +21,31 @@ public class GestioTreballador
 
     }
 
-    public void consultarTreballadors()
+    public ArrayList<Treballador> consultarTreballadors()
     {
         ArrayList<Treballador> listTreballs = new ArrayList<Treballador>();
 
-        
+        try
+        {
+            ConnexioMYSQL con = new ConnexioMYSQL();
+            Statement stat = con.conectar();
+            ResultSet rs = stat.executeQuery("SELECT * FROM treballadors ");
+
+            if(rs.next())
+            {
+                Treballador t = new Treballador();
+                t.setId(rs.getInt("id_treballador"));
+            }
+
+            con.desconectar();
+            return listTreballs;
+        }
+        catch (Exception e)
+        {
+            System.out.println("Error: " + e.getMessage());
+        }
+
+        return null;
     }
 
     public void findTreballador()
