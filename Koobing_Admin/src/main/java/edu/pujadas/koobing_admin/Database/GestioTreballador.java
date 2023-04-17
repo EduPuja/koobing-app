d0fa3ea9295bac6226d14d66b58b90d17d1f01af
EduPC <edpufa@inspalamos.cat>
2023-04-17 23:41:58 +0200

    gestioTreballador

---
+++ Koobing_Admin/src/main/java/edu/pujadas/koobing_admin/Database/GestioTreballador.java
@@ -0,0 +1,35 @@
+package edu.pujadas.koobing_admin.Database;
+
+import edu.pujadas.koobing_admin.Models.Treballador;
+
+import java.util.ArrayList;
+
+public class GestioTreballador
+{
+    public void crearTreballador()
+    {
+
+    }
+    public  void modificarTreballador()
+    {
+
+    }
+    public void eliminarTreballador()
+    {
+
+    }
+
+    public void consultarTreballadors()
+    {
+        ArrayList<Treballador> listTreballs = new ArrayList<Treballador>();
+
+        
+    }
+
+    public void findTreballador()
+    {
+
+    }
+
+
+}
