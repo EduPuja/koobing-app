84ab2b524755c5f5c207cd20a308951dcddfb66e
Eduard <edpufa@inspalamos.cat>
2023-04-19 20:19:39 +0200

    hi ha un error a la pantalla

--- Koobing_Admin/src/main/java/edu/pujadas/koobing_admin/Controllers/TreballadorController.java
+++ Koobing_Admin/src/main/java/edu/pujadas/koobing_admin/Controllers/TreballadorController.java
@@ -2,7 +2,6 @@ package edu.pujadas.koobing_admin.Controllers;
 
 import edu.pujadas.koobing_admin.Database.GestioTreballador;
 import edu.pujadas.koobing_admin.Models.Treballador;
-import edu.pujadas.koobing_admin.Models.Usuari;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -12,7 +11,6 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.TableColumn;
-import javafx.scene.control.TextArea;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 import org.controlsfx.control.tableview2.TableView2;
@@ -63,9 +61,13 @@ public class TreballadorController implements Initializable
         ObservableList<Treballador> obserListUser = FXCollections.observableArrayList(
                 listTreballador
         );
+        PropertyValueFactory<Treballador, Integer> idTreballadorFactory = new PropertyValueFactory<>("id");
 
 
-        idTreballador.setCellValueFactory(new PropertyValueFactory<>("idTreballador"));
+        idTreballador.setCellValueFactory(idTreballadorFactory);
+
+
+
 
         taulaTreballadors.setItems(obserListUser);
     }
