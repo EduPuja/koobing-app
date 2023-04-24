e0e5dba53e1eb854ed659c2aee0b1895be6ef3b3
Eduard <edpufa@inspalamos.cat>
2023-04-24 19:24:20 +0200

    Afegint un treballador

--- Koobing_Admin/src/main/java/edu/pujadas/koobing_admin/Controllers/TreballadorController.java
+++ Koobing_Admin/src/main/java/edu/pujadas/koobing_admin/Controllers/TreballadorController.java
@@ -1,7 +1,9 @@
 package edu.pujadas.koobing_admin.Controllers;
 
 import edu.pujadas.koobing_admin.Database.GestioTreballador;
+import edu.pujadas.koobing_admin.Database.GestioUsuari;
 import edu.pujadas.koobing_admin.Models.Treballador;
+import edu.pujadas.koobing_admin.Models.Usuari;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -10,14 +12,17 @@ import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.TableColumn;
+import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 import org.controlsfx.control.tableview2.TableView2;
 
 import java.net.URL;
 import java.sql.Date;
+import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Optional;
 import java.util.ResourceBundle;
 
 public class TreballadorController implements Initializable
@@ -77,6 +82,96 @@ public class TreballadorController implements Initializable
 
         taulaTreballadors.setItems(obserListUser);
     }
+
+
+
+    /**
+     * Metode que el que fa es inserir un Treballador
+     */
+    public void onInsertarTreballador()
+    {
+        try
+        {
+            // Creacio dels Textes
+            TextInputDialog nomDialeg = new TextInputDialog();
+            TextInputDialog cognomDialeg = new TextInputDialog();
+            PasswordField passwordField = new PasswordField();
+
+            TextInputDialog passwordDialeg = new TextInputDialog(passwordField.getText());
+            TextInputDialog dniDialeg = new TextInputDialog();
+            TextInputDialog emailDialeg = new TextInputDialog();
+            DatePicker dataNaix = new DatePicker();
+            TextInputDialog numSegSocialDialeg = new TextInputDialog();
+            CheckBox isAdminDialeg = new CheckBox();
+
+            //crear el gridpane per posar els 2 camps a l'hora
+            GridPane gridPane = new GridPane();
+            gridPane.setHgap(10);
+            gridPane.setVgap(10);
+
+            gridPane.addRow(0, new Label("Digues el Teu DNI: "),dniDialeg.getEditor());
+            gridPane.addRow(1,new Label("Nou Nom: ") ,nomDialeg.getEditor());
+            gridPane.addRow(2, new Label("Nou Cognom:"), cognomDialeg.getEditor());
+            gridPane.addRow(3, new Label("Data de Naixament:"),dataNaix);
+            gridPane.addRow(4, new Label("Correu Electroinc: "),emailDialeg.getEditor());
+            gridPane.addRow(5, new Label("Contrassenya"),passwordField);
+            gridPane.addRow(6, new Label("Numero de SegSocial: "),numSegSocialDialeg.getEditor());
+            gridPane.addRow(7, new Label("Es Administrador? "),isAdminDialeg);
+
+
+            // Mostrar los dos diálogos en la misma ventana
+            Alert alert = new Alert(Alert.AlertType.NONE);
+
+            alert.setTitle("Modificar dades del Treballador");
+            alert.setHeaderText("Introduïu les noves dades del Treballador");
+            alert.getDialogPane().setContent(gridPane);
+            alert.getButtonTypes().addAll(ButtonType.OK, ButtonType.CANCEL);
+
+            // Esperar a que el usuario presione OK o Cancel
+            Optional<ButtonType> resultat = alert.showAndWait();
+            if (resultat.isPresent() && resultat.get() == ButtonType.OK)
+            {
+                System.out.println("Success Worker!");
+               /* Usuari user = new Usuari();
+                // Actualizar los campos 'nombre' y 'cognom' de la persona seleccionada
+                user.setNom(nomDialeg.getEditor().getText());
+                user.setDni(dniDialeg.getEditor().getText());
+                user.setCognom(cognomDialeg.getEditor().getText());
+                user.setEmail(emailDialeg.getEditor().getText());
+
+                LocalDate data= dataNaix.getValue();
+                Date dataSQL = Date.valueOf(data);
+                user.setDataNaix(dataSQL);
+                user.setPassword(passwordDialeg.getEditor().getText());
+
+
+
+                //actualizar la base de dades
+               // GestioUsuari gestioUsuari = new GestioUsuari();
+                //gestioUsuari.crearUsuari(user);
+
+                // Actualizar la tabla
+                taulaTreballadors.refresh();*/
+            }
+
+
+        }
+        catch (Exception e)
+        {
+            System.out.println("Error: "+ e.getMessage());
+        }
+
+
+
+
+
+
+
+
+    }
+
+
+
 
     // CANVIS DE PANTALLA
 
