6ee67bd508f14f23ac17e539cbcab44f1f698dd7
Eduard <edpufa@inspalamos.cat>
2023-04-24 17:26:20 +0200

    GestioLLibre :white_check_mark:
    Se ha de testejar

--- Koobing_Admin/src/main/java/edu/pujadas/koobing_admin/Database/GestioLlibre.java
+++ Koobing_Admin/src/main/java/edu/pujadas/koobing_admin/Database/GestioLlibre.java
@@ -10,7 +10,10 @@ public class GestioLlibre
 {
 
 
-
+    /**
+     * Metode per crear un llibre
+     * @param l Objecte llibre
+     */
     public void crearLlibre(Llibre l)
     {
         try
@@ -35,6 +38,10 @@ public class GestioLlibre
         }
     }
 
+    /**
+     * Metode per modificar un de la base de dades
+     * @param llibre Objecte llibre
+     */
     public void modificarLlibre(Llibre llibre)
     {
         try
@@ -53,34 +60,102 @@ public class GestioLlibre
         }
     }
 
-    public void eliminarLlibre()
+    public void eliminarLlibre(long ISBN)
     {
         try
         {
             ConnexioMYSQL con = new ConnexioMYSQL();
+            Statement stat = con.conectar();
+            String sql = "DELETE FROM `llibre` WHERE ISBN='"+ISBN+"'";
 
+            if(stat.executeUpdate(sql) == 1)
+            {
+                System.out.println("Llibre eliminado correctamente");
+            }
+            else System.out.println("Llibre no eliminado correctamente");
 
             con.desconectar();
         }
         catch (Exception e)
         {
-
+            System.out.println("Error in Eliminar LLibre:"+ e.getMessage());
         }
     }
-    public void consultarLlibres()
+
+    /**
+     * Metode que et consulta tots els llibres de la base de dades
+     * @return ArrayList de llibres
+     */
+    public ArrayList<Llibre>consultarLlibres()
     {
+        ArrayList<Llibre> llibresList = new ArrayList<Llibre>();
+
+
+        //gestions
+        GestioAutor gestioAutor = new GestioAutor();
+        GestioEditorial gestioEditorial = new GestioEditorial();
+        GestioIdioma gestioIdioma = new GestioIdioma();
+        GestioGenere gestioGenere = new GestioGenere();
         try
         {
             ConnexioMYSQL con = new ConnexioMYSQL();
 
 
+
+            Statement stat = con.conectar();
+
+
+            String query2 = "SELECT * from llibre";
+
+
+            ResultSet rs = stat.executeQuery(query2);
+
+            while (rs.next())
+            {
+                Llibre llibre = new Llibre();
+                // isbn
+                llibre.setISBN(rs.getLong("ISBN"));
+
+                //buscant el autor , editorials, idioma and genere per poder crear un objeto de cada
+
+                Autor autor = gestioAutor.findAutor(rs.getInt("id_autor"));
+                Editorial editor =gestioEditorial.findEditorial(rs.getInt("id_editor"));
+                Idioma idioma = gestioIdioma.findIdioma(rs.getInt("id_idioma"));
+                Genere genere = gestioGenere.findGenere(rs.getInt("id_genere"));
+
+                //afegint tot les dades el objecte llibre
+                llibre.setAutor(autor);
+                llibre.setEditor(editor);
+                llibre.setIdioma(idioma);
+                llibre.setGenere(genere);
+
+                //afegint les dades que falten
+                llibre.setTitol(rs.getString("titol"));
+                llibre.setVersio(rs.getInt("versio"));
+                llibre.setDataPubli(rs.getDate("data_publi"));
+
+                //afegint tots els llibres al arraylist
+                llibresList.add(llibre);
+            }
+
+
             con.desconectar();
+
+            return llibresList;
+
         }
         catch (Exception e)
         {
-
+            System.out.println("Hi ha hagut algun error" + e.getMessage());
         }
+        return null;
     }
+
+
+    /**
+     * Metode que et retorna nomes els 10 primers llibres de la base de dades
+     * @return ArrayList dels 10 primers llibres
+     */
     public ArrayList<Llibre> conusltar10Llibres()
     {
         ArrayList<Llibre> llibresList = new ArrayList<Llibre>();
@@ -148,6 +223,54 @@ public class GestioLlibre
         catch (Exception e)
         {
             System.out.println("Hi ha hagut algun error" + e.getMessage());
+        }
+        return null;
+    }
+
+    /**
+     * Metode per trobar un llibre en concret de la base de dades
+     * @param ISBN numero de llibre
+     * @return Objecte Llibre
+     */
+    public Llibre findLLibre(long ISBN)
+    {
+
+
+        try {
+
+            ConnexioMYSQL con = new ConnexioMYSQL();
+            Statement stat = con.conectar();
+
+            String query = "SELECT * from llibre WHERE ISBN='"+ISBN+"'";
+            ResultSet rs = stat.executeQuery(query);
+
+            if(rs.next())
+            {
+                Llibre llibre = new Llibre();
+
+                llibre.setISBN(rs.getLong("ISBN"));
+
+                // buscant la informacio gracies els gestiors
+                Autor autor = new GestioAutor().findAutor(rs.getInt("id_autor"));
+                Editorial editor =new GestioEditorial().findEditorial(rs.getInt("id_editor"));
+                Idioma idioma = new GestioIdioma().findIdioma(rs.getInt("id_idioma"));
+                Genere genere = new GestioGenere().findGenere(rs.getInt("id_genere"));
+
+                //construinet el llibre
+                llibre.setAutor(autor);
+                llibre.setEditor(editor);
+                llibre.setIdioma(idioma);
+                llibre.setGenere(genere);
+                llibre.setTitol(rs.getString("titol"));
+                llibre.setVersio(rs.getInt("versio"));
+                llibre.setDataPubli(rs.getDate("data_publi"));
+
+                return llibre;
+            }
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
         }
         return null;
     }
