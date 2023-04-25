42e342aee63d7fc30939cfea161c330667e4ed78
Eduard <edpufa@inspalamos.cat>
2023-04-25 19:20:30 +0200

    deixant a punt biblioteca

--- Koobing_Admin/src/main/java/edu/pujadas/koobing_admin/Controllers/BibliotecaController.java
+++ Koobing_Admin/src/main/java/edu/pujadas/koobing_admin/Controllers/BibliotecaController.java
@@ -1,5 +1,169 @@
 package edu.pujadas.koobing_admin.Controllers;
 
-public class BibliotecaController
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class BibliotecaController implements Initializable
 {
+    Parent root;
+    Scene scene;
+    Stage stage;
+
+
+    
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+
+    // CANVIS DE PANTALLA
+
+
+    /**
+     * Funcio per canviar a la pantalla de home
+     * @param event action evnet
+     * @throws Exception exeception
+     */
+    public void switchToHome(ActionEvent event) throws Exception
+    {
+        root = FXMLLoader.load(getClass().getResource("/edu/pujadas/koobing_admin/screens/home.fxml"));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    /**
+     * Funcio per canviar a la pantalla de usuari
+     * @param event action evnet
+     * @throws Exception exeception
+     */
+    public void switchToUsuari(ActionEvent event) throws Exception
+    {
+        root = FXMLLoader.load(getClass().getResource("/edu/pujadas/koobing_admin/screens/usuari.fxml"));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    /**
+     * Funcio per canviar a la pantalla de TREBALLADOR
+     * @param event action evnet
+     * @throws Exception exeception
+     */
+    public void switchToTreballador(ActionEvent event) throws Exception
+    {
+        root = FXMLLoader.load(getClass().getResource("/edu/pujadas/koobing_admin/screens/treballador.fxml"));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+    /**
+     * Funcio per canviar a la pantalla de LLIBRE
+     * @param event action evnet
+     * @throws Exception exeception
+     */
+    public void switchToLlibre(ActionEvent event) throws Exception
+    {
+        root = FXMLLoader.load(getClass().getResource("/edu/pujadas/koobing_admin/screens/llibre.fxml"));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    /**
+     * Funcio per canviar a la pantalla de Autor
+     * @param event action evnet
+     * @throws Exception exeception
+     */
+    public void switchToAutor(ActionEvent event) throws Exception
+    {
+        root = FXMLLoader.load(getClass().getResource("/edu/pujadas/koobing_admin/screens/autor.fxml"));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    /**
+     * Funcio per canviar a la pantalla de Biblioteca
+     * @param event action evnet
+     * @throws Exception exeception
+     */
+    public void switchToBiblioteca(ActionEvent event) throws Exception
+    {
+        root = FXMLLoader.load(getClass().getResource("/edu/pujadas/koobing_admin/screens/biblioteca.fxml"));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    /**
+     * Funcio per canviar a la pantalla de usuari
+     * @param event action evnet
+     * @throws Exception exeception
+     */
+    public void switchToIdioma(ActionEvent event) throws Exception
+    {
+        root = FXMLLoader.load(getClass().getResource("/edu/pujadas/koobing_admin/screens/idioma.fxml"));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    /**
+     * Funcio per canviar a la pantalla de Genere
+     * @param event action evnet
+     * @throws Exception exeception
+     */
+    public void switchToGenere(ActionEvent event) throws Exception
+    {
+        root = FXMLLoader.load(getClass().getResource("/edu/pujadas/koobing_admin/screens/genere.fxml"));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    /**
+     * Funcio per canviar a la pantalla de Editorial
+     * @param event action evnet
+     * @throws Exception exeception
+     */
+    public void switchToEditorial(ActionEvent event) throws Exception
+    {
+        root = FXMLLoader.load(getClass().getResource("/edu/pujadas/koobing_admin/screens/editorial.fxml"));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    /**
+     * Funcio per canviar a la pantalla de Reserves
+     * @param event action evnet
+     * @throws Exception exeception
+     */
+    public void switchToReserva(ActionEvent event) throws Exception
+    {
+        root = FXMLLoader.load(getClass().getResource("/edu/pujadas/koobing_admin/screens/reserva.fxml"));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
 }
