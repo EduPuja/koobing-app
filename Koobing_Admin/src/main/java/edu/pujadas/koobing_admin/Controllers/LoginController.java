a341ab0e47dca106d06f15b6e755f8484a9f5b7c
Eduard <edpufa@inspalamos.cat>
2023-04-27 19:06:27 +0200

    login

--- Koobing_Admin/src/main/java/edu/pujadas/koobing_admin/Controllers/LoginController.java
+++ Koobing_Admin/src/main/java/edu/pujadas/koobing_admin/Controllers/LoginController.java
@@ -1,15 +1,34 @@
 package edu.pujadas.koobing_admin.Controllers;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class LoginController implements Initializable {
+public class LoginController {
 
 
-    @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) {
+    public TextField emailField;
+    public PasswordField passwordField;
 
+
+    public void login(ActionEvent event)
+    {
+        String email = emailField.getText();
+        String password = passwordField.getText();
+
+
+        System.out.println("Email: " + email + " Password: " + password);;
+
+    }
+
+    private boolean isEmailValid(String email) {
+        // Expresión regular para validar correos electrónicos
+        String emailRegex = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Z|a-z]{2,}$";
+        // Validar correo electrónico con la expresión regular
+        return email.matches(emailRegex);
     }
 }
