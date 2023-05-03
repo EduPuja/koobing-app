d1ae8b68b98a10d89deca3f82b0fe647bb8fea2d
Eduard <edpufa@inspalamos.cat>
2023-05-03 18:36:11 +0200

    Creant PasswordUtilites
    Millor separacio

---
+++ Koobing_Admin/src/main/java/edu/pujadas/koobing_admin/Utilities/PasswordUtilites.java
@@ -0,0 +1,59 @@
+package edu.pujadas.koobing_admin.Utilities;
+
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
+
+public class PasswordUtilites {
+
+
+    public static String encryptPassword(String password) {
+        try {
+            byte[] passwordBytes = password.getBytes(StandardCharsets.UTF_8);
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            byte[] hashBytes = digest.digest(passwordBytes);
+            String hash = Base64.getEncoder().encodeToString(hashBytes);
+            password = hash;
+
+            return password;
+        }
+        catch (NoSuchAlgorithmException ex) {
+            throw new RuntimeException("Error al encriptar la contraseña", ex);
+        }
+    }
+
+
+    /**
+     * Metode per comprovar la contrassenya
+     * @param password password a not encrtypted
+     * @param hashedPassword passwordEncrypted
+     * @return true si la contraseña es correcta, false en caso contrario
+     */
+    public static boolean checkPassword(String password, String hashedPassword) {
+        try {
+            String passwordEnpryt = encryptPassword(password);
+
+            System.out.println(passwordEnpryt);
+            if(passwordEnpryt.equals(hashedPassword))
+            {
+                return true;
+            }
+
+            /*byte[] passwordBytes = password.getBytes(StandardCharsets.UTF_8);
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            byte[] hashBytes = digest.digest(passwordBytes);
+            String hash = Base64.getEncoder().encodeToString(hashBytes);
+
+            System.out.println("Checking password : " + password);
+            System.out.println("hash : " + hash);
+            return hash.equals(hashedPassword);*/
+        }
+        catch (Exception ex) {
+
+            System.out.println("Checking password ERROR : " + ex.getMessage());
+            //throw new RuntimeException("Error al comprobar la contraseña", ex);
+        }
+        return false;
+    }
+}
