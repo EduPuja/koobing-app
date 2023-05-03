15370dbf9f4ea479ae7756387aaa336b3be5ebfb
Eduard <edpufa@inspalamos.cat>
2023-05-03 20:22:54 +0200

    works fine
    adding the icon and title

--- Koobing_Admin/src/main/java/edu/pujadas/koobing_admin/App.java
+++ Koobing_Admin/src/main/java/edu/pujadas/koobing_admin/App.java
@@ -23,8 +23,15 @@ public class App extends Application
     public void start(Stage stage) throws Exception
     {
         this.stage = stage;
+        stage.setTitle("Koobing App Admin!");
+
+        //icono
+        String icono ="/edu/pujadas/koobing_admin/img/libro.png";
+        stage.getIcons().add(new Image(App.class.getResource(icono).toExternalForm())); // Agrega el icono
+
         switchToLogin();
-        ;
+        
+
       /*  FXMLLoader loader   = new FXMLLoader(App.class.getResource("/edu/pujadas/koobing_admin/screens/login.fxml"));
         Parent root = loader.load();
         Scene scene = new Scene(root, 1000, 600);
--- Koobing_Admin/src/main/java/edu/pujadas/koobing_admin/Controllers/LoginController.java
+++ Koobing_Admin/src/main/java/edu/pujadas/koobing_admin/Controllers/LoginController.java
@@ -110,7 +110,7 @@ public class LoginController {
 
 
 
- 
+
 
 
 
