9f6b9c112f58c38db21d63faa0e17b3e5dcb5431
Eduard <edpufa@inspalamos.cat>
2023-05-07 17:39:35 +0200

    creant el metode per poder carregar a 2 pantallaa

--- Koobing_Admin/src/main/java/edu/pujadas/koobing_admin/Controllers/MainController.java
+++ Koobing_Admin/src/main/java/edu/pujadas/koobing_admin/Controllers/MainController.java
@@ -4,14 +4,17 @@ import edu.pujadas.koobing_admin.Models.Autor;
 import edu.pujadas.koobing_admin.Models.Llibre;
 import edu.pujadas.koobing_admin.Models.Treballador;
 import edu.pujadas.koobing_admin.Models.Usuari;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
 import javafx.scene.control.TableColumn;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.VBox;
 import org.controlsfx.control.tableview2.TableView2;
 
+import java.io.IOException;
 import java.net.URL;
 import java.sql.Date;
 import java.util.ArrayList;
@@ -26,6 +29,7 @@ public class MainController implements Initializable {
     // treballador stuff
     public ImageView avatarWorker;
     public ImageView logo;
+
     private Treballador worker;
 
 
@@ -66,12 +70,25 @@ public class MainController implements Initializable {
     private final ArrayList<Autor> listAutors = new ArrayList<Autor>();
 
 
+    //container vbox from inserting all tabs and stuff
+    public VBox containerBox;
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         System.out.println("Main Controller....");
 
     }
 
-    
+    public void loadLabelFXML() {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/edu/pujadas/koobing_admin/screens/homeTabs.fxml"));
+            tabPane = loader.load();
+            containerBox.getChildren().add(tabPane);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
 
 }
