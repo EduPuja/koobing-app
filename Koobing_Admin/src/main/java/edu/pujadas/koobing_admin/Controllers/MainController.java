7eb7b2043f01598479b5f0c06f3af824e4c22361
Eduard <edpufa@inspalamos.cat>
2023-05-07 17:54:49 +0200

    woks

--- Koobing_Admin/src/main/java/edu/pujadas/koobing_admin/Controllers/MainController.java
+++ Koobing_Admin/src/main/java/edu/pujadas/koobing_admin/Controllers/MainController.java
@@ -37,38 +37,12 @@ public class MainController implements Initializable {
     //tab pane stuff
     public TabPane tabPane;
     
-    //--- User stuff ---//
-    public Tab usuarisTab;
-    public TableView2<Usuari> taulaUsuaris ;
-    public TableColumn<Usuari,Integer> idUsuari;
-    public TableColumn<Usuari,String> dniColum;
-    public TableColumn<Usuari,String> nomColum;
-    public TableColumn<Usuari,String> cognomColum;
-    public TableColumn<Usuari, Date> dataNaixColum;
-    public TableColumn<Usuari,String > emailColum;
-    private final ArrayList<Usuari> listUsuaris = new ArrayList<Usuari>();
-
-    //--- Book stuff ---//
-    public Tab llibresTab;
-    public TableView2<Llibre> taulaLlibres;
-    public TableColumn<Llibre,Long> isbnColum;
-    public TableColumn<Llibre,String> autorColum;
-    public TableColumn<Llibre,String >editorColum;
-    public TableColumn<Llibre,String> idiomaColum;
-    public TableColumn<Llibre,String> genereColum;
-    public TableColumn<Llibre,String> titolColum;
-    public TableColumn<Llibre,Integer> versioColum;
-    public TableColumn<Llibre, Date> dataPubliColum;
-    private final ArrayList<Llibre> listLlibres = new ArrayList<Llibre>();
+  
 
 
-    //-- author stuff ---//
-    public Tab autorsTap;
-    public TableView2<Autor> taulaAutors;
-    public TableColumn<Autor,Integer> idAutorColum;
-    public TableColumn<Autor,String> nomAutorColum;
-    public TableColumn<Autor,Date> dataNaixAutorColum;
-    private final ArrayList<Autor> listAutors = new ArrayList<Autor>();
+
+
+
 
 
     //container vbox from inserting all tabs and stuff
