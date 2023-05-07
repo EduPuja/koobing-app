0b947fdede332c9976a33c942ea6e7ad91bd083b
Eduard <edpufa@inspalamos.cat>
2023-05-07 17:22:25 +0200

    creacio del main controller
    Controller principal , estic seperant encara més cada apartat

---
+++ Koobing_Admin/src/main/java/edu/pujadas/koobing_admin/Controllers/MainController.java
@@ -0,0 +1,22 @@
+package edu.pujadas.koobing_admin.Controllers;
+
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MainController implements Initializable {
+
+
+    public Button homeBtn ,usuariBtn ,trebaladorBtn, llibreBtn;
+    public Button autorBtn,bibliotecaBtn,idiomaBtn,genereBtn,editorialBtn,reservaBtn;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+
+
+
+}
