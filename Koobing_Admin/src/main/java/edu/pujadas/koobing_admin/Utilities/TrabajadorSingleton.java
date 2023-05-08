9b9a869f2df820b86659e77f34da684a8ceda5e7
Eduard <edpufa@inspalamos.cat>
2023-05-08 16:28:43 +0200

    treballador Sigleton

---
+++ Koobing_Admin/src/main/java/edu/pujadas/koobing_admin/Utilities/TrabajadorSingleton.java
@@ -0,0 +1,27 @@
+
+package edu.pujadas.koobing_admin.Utilities;
+import edu.pujadas.koobing_admin.Models.Treballador;
+
+public class TrabajadorSingleton {
+    private static TrabajadorSingleton instance = null;
+    private Treballador trabajador;
+
+    private TrabajadorSingleton() {}
+
+    public static TrabajadorSingleton getInstance() {
+        if (instance == null) {
+            instance = new TrabajadorSingleton();
+        }
+        return instance;
+    }
+
+    public void setTrabajador(Treballador trabajador) {
+        this.trabajador = trabajador;
+    }
+
+    public Treballador getTrabajador() {
+        return this.trabajador;
+    }
+}
+
+
