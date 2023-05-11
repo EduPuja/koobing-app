db8dd25cc90fbe4516178b48f2d4c028b80e8592
Eduard <edpufa@inspalamos.cat>
2023-05-11 15:02:48 +0200

    creant el StringConverter del user

---
+++ Koobing_Admin/src/main/java/edu/pujadas/koobing_admin/Utilities/UsuariStringConverter.java
@@ -0,0 +1,33 @@
+package edu.pujadas.koobing_admin.Utilities;
+
+import edu.pujadas.koobing_admin.Models.Usuari;
+import javafx.util.StringConverter;
+
+public class UsuariStringConverter extends StringConverter<Usuari> {
+    @Override
+    public String toString(Usuari usuari) {
+        if(usuari !=null)
+        {
+            return usuari.getNom();
+        }
+        return null;
+    }
+
+    @Override
+    public Usuari fromString(String s) {
+        return null;
+    }
+
+    /**
+     * Metode per retornar el id del usuari en cas negatiu retorna -1
+     * @param usuari objecte usuari
+     * @return id del usuari
+     */
+    public int getIdUsuari(Usuari usuari) {
+        if(usuari !=null)
+        {
+            return usuari.getId();
+        }
+        return -1;
+    }
+}
