e25f9a334bf64dc601ece545b3c3b68658588264
Eduard <edpufa@inspalamos.cat>
2023-05-14 16:22:26 +0200

    intentnant fer algo

--- Koobing_App_User/app/src/main/java/edu/pujadas/koobing_app/HomeActivity.java
+++ Koobing_App_User/app/src/main/java/edu/pujadas/koobing_app/HomeActivity.java
@@ -3,13 +3,25 @@ package edu.pujadas.koobing_app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.HorizontalScrollView;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class HomeActivity extends AppCompatActivity {
 
+    BottomNavigationView bottomNavBar ;
+    HorizontalScrollView scrollView;
+    
+    Button
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home_activity);
         setTitle("Home");
+
+
+        bottomNavBar = findViewById(R.id.bottom_navigation_view);
+        scrollView = findViewById(R.id.horizontalScrollView);
+
     }
 }
