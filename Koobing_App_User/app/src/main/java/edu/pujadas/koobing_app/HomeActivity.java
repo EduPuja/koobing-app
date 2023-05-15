cfbbaa398e99f750de3f8bc51d8e5a52a5ceb628
Eduard <edpufa@inspalamos.cat>
2023-05-15 18:05:16 +0200

    intentant fer la response

--- Koobing_App_User/app/src/main/java/edu/pujadas/koobing_app/HomeActivity.java
+++ Koobing_App_User/app/src/main/java/edu/pujadas/koobing_app/HomeActivity.java
@@ -13,12 +13,12 @@ import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
+
 import com.android.volley.toolbox.Volley;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import org.json.JSONObject;
 
-import java.sql.Statement;
 
 
 
@@ -42,35 +42,22 @@ public class HomeActivity extends AppCompatActivity {
         setTitle("Home");
 
         homeLable = findViewById(R.id.homeLabel);
-
-
-
-
-         //RequestQueue query = Volley.newRequestQueue(this);
-
-
-
-
-
-
-
-
         bottomNavBar = findViewById(R.id.bottom_navigation_view);
         scrollView = findViewById(R.id.horizontalScrollView);
 
     }
 
 
-    public void carregarDades(View vista)
-    {
+    public void carregarDades(View vista) {
         String url = "http://192.168.19.0:3000/users";
+
         JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
                 (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
 
                     @Override
                     public void onResponse(JSONObject response) {
-                        homeLable.setText("Succes!");
-                        //Toast.makeText(vista.getContext()).setText("HOla");
+                        homeLable.setText("Success!");
+                        Toast.makeText(HomeActivity.this, "Success!", Toast.LENGTH_SHORT).show();
                         System.out.println("Success");
                     }
                 }, new Response.ErrorListener() {
@@ -78,8 +65,16 @@ public class HomeActivity extends AppCompatActivity {
                     @Override
                     public void onErrorResponse(VolleyError error) {
                         homeLable.setText("Error");
+                        Toast.makeText(HomeActivity.this, "Error!", Toast.LENGTH_SHORT).show();
                         System.out.println("Error");
                     }
                 });
+
+        // Crea una nueva cola de solicitudes de red.
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+
+        // Agrega la solicitud a la cola de solicitudes.
+        requestQueue.add(jsonObjectRequest);
     }
+
 }
