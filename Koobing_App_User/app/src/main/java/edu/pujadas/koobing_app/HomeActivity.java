d8be1957a3f0b782a1050a8b861ba1f8852c48a3
Eduard <edpufa@inspalamos.cat>
2023-05-15 17:05:58 +0200

    intentnat fer algo

--- Koobing_App_User/app/src/main/java/edu/pujadas/koobing_app/HomeActivity.java
+++ Koobing_App_User/app/src/main/java/edu/pujadas/koobing_app/HomeActivity.java
@@ -6,7 +6,15 @@ import android.os.Bundle;
 import android.widget.HorizontalScrollView;
 import android.widget.TextView;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import org.json.JSONObject;
 
 import java.sql.Statement;
 
@@ -20,7 +28,10 @@ public class HomeActivity extends AppCompatActivity {
     HorizontalScrollView scrollView;
 
 
-
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+        super.onPointerCaptureChanged(hasCapture);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +40,25 @@ public class HomeActivity extends AppCompatActivity {
         setTitle("Home");
 
         homeLable = findViewById(R.id.homeLabel);
+
+        String url = "http://localhost:3000/connect";
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
+                (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        System.out.println("Susscess");
+                    }
+                }, new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        System.out.println("Error ; (");
+
+                    }
+                });
+
+         //RequestQueue query = Volley.newRequestQueue(this);
 
 
 
