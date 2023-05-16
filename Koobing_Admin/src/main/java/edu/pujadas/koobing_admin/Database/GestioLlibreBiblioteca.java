17678893e3097657f76fa6bdb6f807b910132936
Eduard <edpufa@inspalamos.cat>
2023-05-16 15:24:05 +0200

    creant la consulta de llibre bilblioteca

--- Koobing_Admin/src/main/java/edu/pujadas/koobing_admin/Database/GestioLlibreBiblioteca.java
+++ Koobing_Admin/src/main/java/edu/pujadas/koobing_admin/Database/GestioLlibreBiblioteca.java
@@ -1,6 +1,12 @@
 package edu.pujadas.koobing_admin.Database;
 
+import edu.pujadas.koobing_admin.Models.Biblioteca;
 import edu.pujadas.koobing_admin.Models.Llibre;
+import edu.pujadas.koobing_admin.Models.LlibreBiblio;
+
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
 
 public class GestioLlibreBiblioteca {
 
@@ -18,5 +24,39 @@ public class GestioLlibreBiblioteca {
     public void eliminarLlibreBiblioteca(Llibre llibre)
     {
 
+    }
+
+    public ArrayList<LlibreBiblio> consultarLlibreBiblioteca()
+    {
+        try
+        {
+            //gestors
+            GestioLlibre gestioLlibre = new GestioLlibre();
+            GestioBiblioteca gestioBiblioteca = new GestioBiblioteca();
+          ArrayList<LlibreBiblio>  listBiblioBook=  new ArrayList<LlibreBiblio>();
+
+            ConnexioMYSQL con = new ConnexioMYSQL();
+            Statement stat = con.conectar();
+            String query ="SELECT * FROM biblio_llibre";
+            ResultSet rs = stat.executeQuery(query);
+            while (rs.next())
+            {
+                LlibreBiblio llibreBiblio = new LlibreBiblio();
+                Llibre book = gestioLlibre.findLLibre(rs.getLong("ISBN"));
+                Biblioteca b = gestioBiblioteca.findBiblioteca(rs.getInt("id_biblioteca"));
+                llibreBiblio.setBook(book);
+                llibreBiblio.setBiblioteca(b);
+
+                listBiblioBook.add(llibreBiblio);
+                return  listBiblioBook;
+
+            }
+        }
+        catch (Exception e)
+        {
+            System.out.println("Error consulting relation between book and library: "+ e.getMessage());
+        }
+
+        return null;
     }
 }
