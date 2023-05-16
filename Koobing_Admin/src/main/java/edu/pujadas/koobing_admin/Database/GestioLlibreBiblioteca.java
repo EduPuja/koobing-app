eb933a63ba28b53c65818f2518748e4532e29562
Eduard <edpufa@inspalamos.cat>
2023-05-16 15:03:46 +0200

    nou gestio

---
+++ Koobing_Admin/src/main/java/edu/pujadas/koobing_admin/Database/GestioLlibreBiblioteca.java
@@ -0,0 +1,22 @@
+package edu.pujadas.koobing_admin.Database;
+
+import edu.pujadas.koobing_admin.Models.Llibre;
+
+public class GestioLlibreBiblioteca {
+
+    public void crearLlibreBiblioteca(Llibre llibre)
+    {
+
+    }
+
+    public void modificarLlibreBiblioteca(Llibre llibre)
+    {
+
+    }
+
+
+    public void eliminarLlibreBiblioteca(Llibre llibre)
+    {
+
+    }
+}
