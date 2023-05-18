3177660d3082128ce60a2ac085c1eb06f68d3163
Eduard <edpufa@inspalamos.cat>
2023-05-18 17:28:42 +0200

    treient la base 64

--- ApiRestAndoird/src/controllers/controllerUsuario.js
+++ ApiRestAndoird/src/controllers/controllerUsuario.js
@@ -1,31 +1,26 @@
-const conection = require('../database/conexio');
-
+const conection = require("../database/conexio");
 
 function getAllUsers(req, res) {
+  console.log("\nOpinguent tots els usuaris");
+  const sql = "SELECT * FROM usuari";
 
-  console.log("\nOpinguent tots els usuaris")
-    const sql = "SELECT * FROM usuari";
-  
-    conection.query(sql, (error, result) => {
-      if (error) {
-        console.error("Error al obtener la lista de usuarios: ", error);
-        res.status(500).send("Error interno del servidor");
-      } 
-      else {
-        const users = result.map((user) => {
-          return {
-            ...user,
-            avatar: user.avatar.toString("base64"),
-          };
-        });
-  
-        res.json(users);
-      }
-    });
-  }
-
-
-  
-module.exports = {
-    getAllUsers
+  conection.query(sql, (error, result) => {
+    if (error) {
+      console.error("Error al obtener la lista de usuarios: ", error);
+      res.status(500).send("Error interno del servidor");
+    } else {
+      /*const users = result.map((user) => {
+        return {
+          ...user,
+          avatar: user.avatar.toString("base64"),
+        };
+      });
+        */
+      res.json(result);
+    }
+  });
 }
+
+module.exports = {
+  getAllUsers,
+};
