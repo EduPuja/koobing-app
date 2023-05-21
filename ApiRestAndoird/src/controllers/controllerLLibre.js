2563f5f8450f4820bddcd1806886da6332c662bb
Eduard <edpufa@inspalamos.cat>
2023-05-21 23:57:33 +0200

    llibres nomes

--- ApiRestAndoird/src/controllers/controllerLLibre.js
+++ ApiRestAndoird/src/controllers/controllerLLibre.js
@@ -6,8 +6,9 @@ function getAllLlibres(req, res) {
     console.log("\nGetting all the books in the database");
 
   //consulta inner join amb tota la informacio del llibre i la biblioteca
-  const sql =
-    "SELECT bl.id, bl.id_biblioteca, b.nom_biblio, l.ISBN, l.titol, l.id_autor, l.id_editor, l.id_idioma, l.id_genere, l.versio, l.data_publi,bl.stock FROM biblio_llibre bl INNER JOIN llibre l ON bl.ISBN = l.ISBN INNER JOIN biblioteca b ON b.id_biblioteca = bl.id_biblioteca";
+  /*const sql =
+    "SELECT bl.id, bl.id_biblioteca, b.nom_biblio, l.ISBN, l.titol, l.id_autor, l.id_editor, l.id_idioma, l.id_genere, l.versio, l.data_publi,bl.stock FROM biblio_llibre bl INNER JOIN llibre l ON bl.ISBN = l.ISBN INNER JOIN biblioteca b ON b.id_biblioteca = bl.id_biblioteca";*/
+    const sql = "SELECT * from llibre ";
 
   connection.query(sql, function (error, result) {
     if (error) {
