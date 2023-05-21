0d006c8c490995cde50ab2253afd9a1e1fa66990
Eduard <edpufa@inspalamos.cat>
2023-05-21 22:31:03 +0200

    fent el loader de llibres

--- Koobing_App_User/app/src/main/java/edu/pujadas/koobing_app/Loaders/LlibreBibliotecaLoader.java
+++ Koobing_App_User/app/src/main/java/edu/pujadas/koobing_app/Loaders/LlibreBibliotecaLoader.java
@@ -1,7 +1,15 @@
 package edu.pujadas.koobing_app.Loaders;
 
+import java.util.List;
+
+import edu.pujadas.koobing_app.Models.LlibreBiblioteca;
+import edu.pujadas.koobing_app.Models.Usuari;
+import edu.pujadas.koobing_app.Services.ApiCallback;
 import edu.pujadas.koobing_app.Services.LlibreService;
 import edu.pujadas.koobing_app.Services.UserService;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -19,5 +27,29 @@ public class LlibreBibliotecaLoader {
 
 
         llibreService = retrofit.create(LlibreService.class);
+    }
+
+
+    public void obtenerLibrosfinal  (final ApiCallback<List<LlibreBiblioteca>> callback)
+    {
+        Call <List<LlibreBiblioteca>> call = llibreService.getAllBooks();
+        call.enqueue(new Callback<List<LlibreBiblioteca>>() {
+
+            @Override
+            public void onResponse(Call<List<LlibreBiblioteca>> call, Response<List<LlibreBiblioteca>> response) {
+                if (response.isSuccessful()) {
+                    List<LlibreBiblioteca> librosBiblioteca = response.body();
+                    callback.onSuccess(librosBiblioteca);
+                }
+                else {
+                    callback.onError(response.code());
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<LlibreBiblioteca>> call, Throwable t) {
+                callback.onFailure(t);
+            }
+        });
     }
 }
