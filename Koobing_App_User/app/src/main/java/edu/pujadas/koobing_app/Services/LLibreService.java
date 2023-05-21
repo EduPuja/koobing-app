a4ffd75872e6fff2b62718068a0bd01dc5160504
Eduard <edpufa@inspalamos.cat>
2023-05-21 22:25:50 +0200

    acabant de crear la interficia

--- Koobing_App_User/app/src/main/java/edu/pujadas/koobing_app/Services/LLibreService.java
+++ Koobing_App_User/app/src/main/java/edu/pujadas/koobing_app/Services/LLibreService.java
@@ -2,11 +2,12 @@ package edu.pujadas.koobing_app.Services;
 
 import java.util.List;
 
+import edu.pujadas.koobing_app.Models.LlibreBiblioteca;
 import retrofit2.Call;
 import retrofit2.http.GET;
 
 public interface LLibreService {
 
     @GET("/books")
-    Call<List<Ll>>
+    Call<List<LlibreBiblioteca>> getAllBooks();
 }
