3aba4dc84c6480d8a7f25e6dd70d0acbea6264b9
Eduard <edpufa@inspalamos.cat>
2023-05-21 22:19:22 +0200

    fent el getAllLlibres

--- ApiRestAndoird/src/controllers/controllerLLibre.js
+++ ApiRestAndoird/src/controllers/controllerLLibre.js
@@ -1,14 +1,26 @@
 const connection = require("../database/conexio");
 
+function getAllLlibres(req, res) {
 
 
-function getAllLlibres(req,res)
-{
+    console.log("\nGetting all the books in the database");
 
+  //consulta inner join amb tota la informacio del llibre i la biblioteca
+  const sql =
+    "SELECT bl.id, bl.id_biblioteca, b.nom_biblio, l.ISBN, l.titol, l.id_autor, l.id_editor, l.id_idioma, l.id_genere, l.versio, l.data_publi,bl.stock FROM biblio_llibre bl INNER JOIN llibre l ON bl.ISBN = l.ISBN INNER JOIN biblioteca b ON b.id_biblioteca = bl.id_biblioteca";
+
+  connection.query(sql, function (error, result) {
+    if (error) {
+        console.error("Error al obtener toda la inforamcion de la biblioteca: ", error);
+        res.status(500).send("Error interno del servidor");
+    }
+    else
+    {
+        res.json(result);
+    }
+  });
 }
-
-
 
 module.exports = {
-    getAllLlibres
-}
+  getAllLlibres,
+};
