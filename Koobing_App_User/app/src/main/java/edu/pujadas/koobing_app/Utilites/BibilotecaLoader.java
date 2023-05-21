8bd6c3dd8b31fe7c58af02f362fab21efdf05aba
Eduard <edpufa@inspalamos.cat>
2023-05-21 17:40:27 +0200

    creant la classe per carregar les dades

---
+++ Koobing_App_User/app/src/main/java/edu/pujadas/koobing_app/Utilites/BibilotecaLoader.java
@@ -0,0 +1,9 @@
+package edu.pujadas.koobing_app.Utilites;
+
+import edu.pujadas.koobing_app.Services.BibliotecaService;
+
+public class BibilotecaLoader {
+
+    private BibliotecaService serviBiblioteca;
+
+}
