68e2099c6fe5574c3e414a0eaaae5f2c44381b35
Eduard <edpufa@inspalamos.cat>
2023-05-21 15:32:25 +0200

    fent user api
    no se si el @get estara correcte

--- Koobing_App_User/app/src/main/java/edu/pujadas/koobing_app/Utilites/UserService.java
+++ Koobing_App_User/app/src/main/java/edu/pujadas/koobing_app/Utilites/UserService.java
@@ -5,11 +5,15 @@ import java.util.List;
 import edu.pujadas.koobing_app.Models.Usuari;
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Path;
 
 public interface UserService {
 
     String ruta = "http://192.168.0.33:3000/users/";
     @GET(ruta)
     Call<List<Usuari>>getUsuaris();
+
+    @GET("user/{email}")
+    Call<Usuari> getUserByEmail(@Path("email") String email);
 
 }
