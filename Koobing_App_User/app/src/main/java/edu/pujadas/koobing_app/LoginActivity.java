b76d8d9bb7565603c35120f3caf32c9f0bfb54c7
Eduard <edpufa@inspalamos.cat>
2023-05-21 15:36:57 +0200

    provant la el nou login

--- Koobing_App_User/app/src/main/java/edu/pujadas/koobing_app/LoginActivity.java
+++ Koobing_App_User/app/src/main/java/edu/pujadas/koobing_app/LoginActivity.java
@@ -15,6 +15,8 @@ import java.util.List;
 
 import edu.pujadas.koobing_app.Models.Usuari;
 import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -90,6 +92,39 @@ public class LoginActivity extends AppCompatActivity {
         UserService userService = retrofit.create(UserService.class);
 
         Call<Usuari> call = userService.getUserByEmail(email);
+
+        call.enqueue(new Callback<Usuari>() {
+
+            @Override
+            public void onResponse(Call<Usuari> call, Response<Usuari> response) {
+                if (response.isSuccessful()) {
+                    Usuari user = response.body();
+                    if (user != null) {
+                        // comprovar si la password entrada es correcta
+                        if(Validator.checkPassword(password, user.getPassword()))
+                        {
+                            Toast.makeText(getApplicationContext(),"SUCCES EL USUARI SE HA TROBAT!",Toast.LENGTH_SHORT).show();
+                        }
+
+                    }
+                    else {
+                        // Toast el usuari no exgisteix
+                        Toast.makeText(getApplicationContext(),"El usuari no s'ha trobat",Toast.LENGTH_SHORT).show();
+                    }
+                }
+                else {
+                    // Error en la respuesta del servidor
+                    Toast.makeText(getApplicationContext(),"Error a la resposta del servidor",Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<Usuari> call, Throwable t) {
+                System.out.println("Failure: " + t.getMessage());
+            }
+
+
+        }); //end callback enque
 
 
 
