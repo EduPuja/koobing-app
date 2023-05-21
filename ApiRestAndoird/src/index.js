1ac89b0b57664404cb2f3d9f2e9be0e52a27743c
Eduard <edpufa@inspalamos.cat>
2023-05-21 22:09:37 +0200

    creant les rutes i configurant tot per fer el llibre
    peso pesado

--- ApiRestAndoird/src/index.js
+++ ApiRestAndoird/src/index.js
@@ -1,24 +1,28 @@
 const express = require("express");
 const app = express();
 const bodyParser = require('body-parser');
-//const connection = require("./database/conexio");
 const controllerUsuario= require("./controllers/controllerUsuario");
 const controllerBiblio = require("./controllers/controllerBiblioteca");
+const controllerLlibre = require("./controllers/controllerLLibre");
 
 
-// Configura el middleware body-parser para analizar los datos enviados en las solicitudes
+// Configuration de la api
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 
-//utilzar jsons
-//app.use(express.json());
+
+
+
+//rutes 
 app.get("/users",controllerUsuario.getAllUsers);
 
 app.get("/user/:email",controllerUsuario.getUserByEmail)
 
 app.get("/biblioteques",controllerBiblio.getAllBiblioteques)
 
+app.get("/books",controllerLlibre.getAllLlibres)
+
 app.listen(3000, () => {
-  console.log("La aplicación está corriendo en el puerto 3000.");
+  console.log("L' aplicació està funcionant  per el port: 3000");
 });
