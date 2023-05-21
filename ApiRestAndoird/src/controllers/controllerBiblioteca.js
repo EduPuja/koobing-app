12c69a3b8d2d968038f48cfe1e1608e2c822f258
Eduard <edpufa@inspalamos.cat>
2023-05-21 17:20:29 +0200

    fent la connexio i si no hi ha error ,retorna un json

--- ApiRestAndoird/src/controllers/controllerBiblioteca.js
+++ ApiRestAndoird/src/controllers/controllerBiblioteca.js
@@ -4,6 +4,19 @@ const connection = require("../database/conexio");
 function getAllBiblioteques(req,res)
 {
     console.log("Getting all the Libraris")
+    const sql = "SELECT * FROM biblioteca"
+
+    connection.query(sql,(error, result) => {
+
+        if(error) 
+        {
+            console.error("Error al obtener la lista de BIBLIOTECAS: ", error);
+            res.status(500).send("Error interno del servidor");
+        }
+        else{
+           res.json(result);
+        }
+    });
 }
 
 
