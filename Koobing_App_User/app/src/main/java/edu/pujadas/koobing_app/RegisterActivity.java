f8b39e6cb81aa190b186b2c5a6997231d5f356f8
Eduard <edpufa@inspalamos.cat>
2023-05-22 19:04:59 +0200

    intentant fer el registre

--- Koobing_App_User/app/src/main/java/edu/pujadas/koobing_app/RegisterActivity.java
+++ Koobing_App_User/app/src/main/java/edu/pujadas/koobing_app/RegisterActivity.java
@@ -3,6 +3,7 @@ package edu.pujadas.koobing_app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -11,5 +12,15 @@ public class RegisterActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.register_activity);
         setTitle("Register");
+    }
+
+
+    /**
+     * Metode per doanr d'alta un usuairo
+     * @param v vista
+     */
+    public void onRegisterClick(View v)
+    {
+        
     }
 }
