9f64a332f039c828b0f6333dfd2e823e707ce27e
Eduard <edpufa@inspalamos.cat>
2023-05-22 22:39:52 +0200

    millorant la api

--- ApiRestAndoird/src/controllers/controllerReserva.js
+++ ApiRestAndoird/src/controllers/controllerReserva.js
@@ -1,16 +1,17 @@
 const connection = require("../database/conexio");
 
 
-function setReserva(req,res)
-{
-    console.log("\nCreant una reserva")
-    const reserva = req.body;
 
-    const query = "INSERT INTO reserves (id_reserva, id_usuari, id_treballador, id_biblioteca, ISBN, data_inici, data_fi) VALUES (?, ?, ?, ?, ?, ?, ?)";
+function setReserva(req, res) {
+  console.log("\nCreant una reserva");
+  const reserva = req.body;
 
-    //definir els valors de la reserva
-    const valores = [reserva.id_reserva, reserva.id_usuari, reserva.id_treballador, reserva.id_biblioteca, reserva.ISBN, reserva.data_inici, reserva.data_fi];
-    // Ejecuta la consulta en la conexión a la base de datos
+  const query = "INSERT INTO reserves (id_reserva, id_usuari, id_treballador, id_biblioteca, ISBN, data_inici, data_fi) VALUES (?, ?, ?, ?, ?, ?, ?)";
+
+  // Definir los valores de la reserva
+  const valores = [reserva.id_reserva, reserva.id_usuari, reserva.id_treballador, reserva.id_biblioteca, reserva.ISBN, reserva.data_inici, reserva.data_fi];
+
+  // Ejecuta la consulta en la conexión a la base de datos
   connection.query(query, valores, (error, results) => {
     if (error) {
       console.error("Error al insertar la reserva:", error);
@@ -20,9 +21,10 @@ function setReserva(req,res)
       res.status(200).json({ message: "Reserva insertada correctamente" });
     }
   });
-
 }
+
 
 module.exports = {
-    setReserva
-}
+  setReserva,
+};
+
