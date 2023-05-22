4fb62ce6264ab92faf3a136cf64e504d46937e78
Eduard <edpufa@inspalamos.cat>
2023-05-22 20:29:20 +0200

    provant de fer la reserva

--- Koobing_App_User/app/src/main/java/edu/pujadas/koobing_app/Adapters/CarouselAdapter.java
+++ Koobing_App_User/app/src/main/java/edu/pujadas/koobing_app/Adapters/CarouselAdapter.java
@@ -5,14 +5,26 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.viewpager.widget.PagerAdapter;
 
+import java.sql.Date;
+import java.time.LocalDate;
 import java.util.List;
 
 import edu.pujadas.koobing_app.Models.Llibre;
+import edu.pujadas.koobing_app.Models.Reserva;
+import edu.pujadas.koobing_app.Models.Treballador;
 import edu.pujadas.koobing_app.R;
+import edu.pujadas.koobing_app.Services.ReservaService;
+import okhttp3.OkHttpClient;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class CarouselAdapter extends PagerAdapter {
 
@@ -47,7 +59,55 @@ public class CarouselAdapter extends PagerAdapter {
 
             bookTitleTextView.setText(book.getTitol());
             readButton.setOnClickListener(v -> {
-              
+
+                Reserva reserva = new Reserva();
+
+                reserva.setLlibre(book);
+                //reserva.setBiblio();
+                Treballador administrador = new Treballador();
+                administrador.setId(1);
+                administrador.setNom("Admin");
+                administrador.setEmail("administrador@mail.com");
+                administrador.setAdmin(true);
+
+                reserva.setTreballador(administrador);
+                Date dataInici = Date.valueOf(String.valueOf(LocalDate.now()));
+                Date dataFi = Date.valueOf(String.valueOf(LocalDate.now().plusMonths(1)));
+                reserva.setDataInici(dataInici);
+
+                String url = "http://192.168.16.254:3000/reservarLlibre/";
+                OkHttpClient.Builder httpClient = new OkHttpClient.Builder();
+                Retrofit retrofit = new Retrofit.Builder()
+                        .baseUrl("") // URL base de tu API
+                        .client(httpClient.build())
+                        .addConverterFactory(GsonConverterFactory.create())
+                        .build();
+
+
+                ReservaService reservaService = retrofit.create(ReservaService.class);
+                Call<Void> call = reservaService.hacerReserva(reserva);
+                call.enqueue(new Callback<Void>() {
+
+                    @Override
+                    public void onResponse(Call<Void> call, Response<Void> response) {
+                        if(response.isSuccessful())
+                        {
+                            System.out.println("Reserva creada ");
+                            Toast.makeText(container.getContext(), "El llibre s'ha reservat correctamet", Toast.LENGTH_SHORT).show();
+
+                        }
+                        else
+                        {
+                            Toast.makeText(container.getContext(), "Error al realizar la reserva", Toast.LENGTH_SHORT).show();
+
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(Call<Void> call, Throwable t) {
+                        Toast.makeText(container.getContext(), "Error en la solicitud", Toast.LENGTH_SHORT).show();
+                    }
+                }); //end call
             });
         }
         else
