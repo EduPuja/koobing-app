01179853385a5ffd749f457fb58389350d8fe547
Eduard <edpufa@inspalamos.cat>
2023-05-22 18:36:24 +0200

    posant el registre

--- Koobing_App_User/app/src/main/java/edu/pujadas/koobing_app/RegisterActivity.java
+++ Koobing_App_User/app/src/main/java/edu/pujadas/koobing_app/RegisterActivity.java
@@ -10,5 +10,6 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.register_activity);
+        setTitle("Register");
     }
 }
