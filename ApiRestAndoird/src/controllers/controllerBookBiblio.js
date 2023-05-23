59f751dcf8748e5ed4897a14f4c0c3ff6a9a9d11
Eduard <edpufa@inspalamos.cat>
2023-05-23 13:26:16 +0200

    creant el resulta de llibre biblioteca

--- ApiRestAndoird/src/controllers/controllerBookBiblio.js
+++ ApiRestAndoird/src/controllers/controllerBookBiblio.js
@@ -2,7 +2,22 @@ const connection = require("../database/conexio");
 
 function getAllBooksWithBiblioteca(req,res)
 {
+    console.log("\nGetting all libraries with books")
 
+    const query = "select  * from biblio_llibre";
+
+    connection.query(query ,function(error,result){
+
+        if(error)
+        {
+            console.error("Error al obtener la lista de de biblio_llibre: ", error.message);
+            res.status(500).send("Error interno del servidor");
+        }
+        else{
+            res.json(result);
+        }
+
+    });
 }
 
 module.exports ={
