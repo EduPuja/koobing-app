3d1697a45c59a4dffc58d0e0ce3d85be05acff20
Eduard <edpufa@inspalamos.cat>
2023-05-23 16:11:09 +0200

    afegint tot el que interactua amb el frontedn

--- Koobing_App_User/app/src/main/java/edu/pujadas/koobing_app/RegisterActivity.java
+++ Koobing_App_User/app/src/main/java/edu/pujadas/koobing_app/RegisterActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import edu.pujadas.koobing_app.Models.Usuari;
@@ -18,11 +20,24 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class RegisterActivity extends AppCompatActivity {
 
     private RegisterService registerService;
+
+    EditText dniField ,nomField,cognomField,dataNaixField,correuField,passwordField;
+    Button registerBtn;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.register_activity);
         setTitle("Register");
+
+        dniField = findViewById(R.id.dniField);
+        nomField = findViewById(R.id.nomField);
+        cognomField = findViewById(R.id.cognomField);
+        dataNaixField = findViewById(R.id.dataNaixField);
+        correuField =  findViewById(R.id.correuField);
+        passwordField = findViewById(R.id.passwordField);
+
     }
 
 
