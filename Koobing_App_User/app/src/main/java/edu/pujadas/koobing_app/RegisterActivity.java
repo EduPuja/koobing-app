1a13f719f4405f62c05d637dcd7c6dab9612e6c9
Eduard <edpufa@inspalamos.cat>
2023-05-23 16:05:55 +0200

    creant la forma de registrer

--- Koobing_App_User/app/src/main/java/edu/pujadas/koobing_app/RegisterActivity.java
+++ Koobing_App_User/app/src/main/java/edu/pujadas/koobing_app/RegisterActivity.java
@@ -4,9 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
+
+import edu.pujadas.koobing_app.Models.Usuari;
+import edu.pujadas.koobing_app.Services.RegisterService;
+import edu.pujadas.koobing_app.Services.UserService;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RegisterActivity extends AppCompatActivity {
 
+    private RegisterService registerService;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,5 +33,41 @@ public class RegisterActivity extends AppCompatActivity {
     public void onRegisterClick(View v)
     {
 
+
+
+
+
+    }
+
+    private void sendUserPost(Usuari usuari)
+    {
+
+        //ip instutut
+        String url = "http://192.168.16.254:3000/register/";
+        //ip home
+        // String url ="http://192.168.0.33:3000/register/";
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(url)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+
+        registerService = retrofit.create(RegisterService.class);
+
+        Call<Void> call = registerService.register(usuari);
+
+
+        call.enqueue(new Callback<Void>() {
+            @Override
+            public void onResponse(Call<Void> call, Response<Void> response) {
+                Toast.makeText(getApplicationContext(), "T'has donat d'alta correctament ! "+usuari.getNom() +" "+usuari.getCognom(), Toast.LENGTH_SHORT).show();;
+            }
+
+            @Override
+            public void onFailure(Call<Void> call, Throwable t) {
+                Toast.makeText(getApplicationContext(), "Hi ha hagut un error \uD83D\uDE14", Toast.LENGTH_SHORT).show();;
+            }
+        });
     }
 }
