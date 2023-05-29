99998d9456aab37cd4f1f245b67839183eb86d96
Eduard <edpufa@inspalamos.cat>
2023-05-29 16:33:31 +0200

    millorant el searcbar

--- Koobing_App_User/app/src/main/java/edu/pujadas/koobing_app/SearchActivity.java
+++ Koobing_App_User/app/src/main/java/edu/pujadas/koobing_app/SearchActivity.java
@@ -1,14 +1,68 @@
 package edu.pujadas.koobing_app;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import edu.pujadas.koobing_app.Models.Llibre;
 
 public class SearchActivity extends AppCompatActivity {
 
+    private SearchView searchView;
+
+    private List<Llibre> listLlibres;
+    // recilcerview , layout inflanter y adapter
+    private RecyclerView recyclerView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.search_activity);
+
+        //instanciantel arraylist
+        listLlibres = new ArrayList<Llibre>();
+        searchView = findViewById(R.id.searchView);
+        searchView.clearFocus();
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+
+                filterList(newText);
+                return false;
+            }
+        });
+    }
+
+    private void filterList(String newText) {
+        List<Llibre> filtreredList = new ArrayList<Llibre>();
+        for (Llibre l : listLlibres)
+        {
+            if(l.getTitol().toLowerCase().contains(newText.toLowerCase()))
+            {
+                //afegint el llibre filtrat
+                filtreredList.add(l);
+            }
+
+
+        }
+
+        if(filtreredList.isEmpty()){
+            Toast.makeText(this,"Llibre no trobat ",Toast.LENGTH_SHORT).show();
+        }
+        else
+        {
+            //todo adapter -> setFiltredList(filtreredList)
+        }
     }
 }
