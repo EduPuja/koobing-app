095608a1ed71ceef80dd9beeca87fc312786dfb9
Eduard <edpufa@inspalamos.cat>
2023-05-30 16:13:46 +0200

    millorant la consulta
    nomes els que tinguin stock

--- ApiRestAndoird/src/controllers/controllerLLibre.js
+++ ApiRestAndoird/src/controllers/controllerLLibre.js
@@ -8,7 +8,7 @@ function getAllLlibres(req, res) {
   //consulta inner join amb tota la informacio del llibre i la biblioteca
   /*const sql =
     "SELECT bl.id, bl.id_biblioteca, b.nom_biblio, l.ISBN, l.titol, l.id_autor, l.id_editor, l.id_idioma, l.id_genere, l.versio, l.data_publi,bl.stock FROM biblio_llibre bl INNER JOIN llibre l ON bl.ISBN = l.ISBN INNER JOIN biblioteca b ON b.id_biblioteca = bl.id_biblioteca";*/
-    const sql = "SELECT * from llibre ";
+    const sql = "SELECT * from llibre where stock > 0";
 
   connection.query(sql, function (error, result) {
     if (error) {
