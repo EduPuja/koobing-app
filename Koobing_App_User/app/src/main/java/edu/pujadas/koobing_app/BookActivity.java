021ee86c04ba0c4d0d1a246fdc6a7fe2b8c8076d
Eduard <edpufa@inspalamos.cat>
2023-05-31 14:50:50 +0200

    provant el GSON

--- Koobing_App_User/app/src/main/java/edu/pujadas/koobing_app/BookActivity.java
+++ Koobing_App_User/app/src/main/java/edu/pujadas/koobing_app/BookActivity.java
@@ -3,12 +3,27 @@ package edu.pujadas.koobing_app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.google.gson.Gson;
+
+import edu.pujadas.koobing_app.Models.Llibre;
 
 public class BookActivity extends AppCompatActivity {
 
+    TextView titolLlibre;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.book_activity);
+
+        titolLlibre = findViewById(R.id.nomLlibre);
+
+        //recollint el intent en format json y transformant-lo a objecte
+        String jsonLlibre = getIntent().getStringExtra("llibreJson");
+        Gson gson = new Gson();
+        Llibre llibre = gson.fromJson(jsonLlibre, Llibre.class);
+
+        titolLlibre.setText(llibre.getTitol());
     }
 }
