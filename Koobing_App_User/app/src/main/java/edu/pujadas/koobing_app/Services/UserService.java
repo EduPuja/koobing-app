0a952d942e496af915e87d4fde8c9a017ec7e5dd
Eduard <edpufa@inspalamos.cat>
2023-05-31 15:24:44 +0200

    posant el registre a userService

--- Koobing_App_User/app/src/main/java/edu/pujadas/koobing_app/Services/UserService.java
+++ Koobing_App_User/app/src/main/java/edu/pujadas/koobing_app/Services/UserService.java
@@ -5,7 +5,9 @@ import java.util.List;
 import edu.pujadas.koobing_app.Models.Usuari;
 import okhttp3.ResponseBody;
 import retrofit2.Call;
+import retrofit2.http.Body;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
 import retrofit2.http.Path;
 
 public interface UserService {
@@ -17,5 +19,10 @@ public interface UserService {
 
     @GET("/user/{email}")
     Call<ResponseBody> getUserByEmail(@Path("email") String email);
+
+
+    @POST("/register")
+    Call<Void> register(@Body Usuari userRegisted);
+
 
 }
