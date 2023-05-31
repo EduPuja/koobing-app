8e04b386ad3db73f3673cb4f2c6d98a835e95dfc
Eduard <edpufa@inspalamos.cat>
2023-05-31 15:03:05 +0200

    posant tot el frontend

--- Koobing_App_User/app/src/main/java/edu/pujadas/koobing_app/BookActivity.java
+++ Koobing_App_User/app/src/main/java/edu/pujadas/koobing_app/BookActivity.java
@@ -3,6 +3,7 @@ package edu.pujadas.koobing_app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.google.gson.Gson;
@@ -11,13 +12,24 @@ import edu.pujadas.koobing_app.Models.Llibre;
 
 public class BookActivity extends AppCompatActivity {
 
-    TextView titolLlibre;
+    TextView titolLlibre,autorName,editorial,genere,idioma,edicio,dataPublicacio;
+    ImageView logoLlibre;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.book_activity);
 
+
+        //find all elements in xml file
         titolLlibre = findViewById(R.id.nomLlibre);
+        autorName = findViewById(R.id.autorName);
+        editorial = findViewById(R.id.editorialName);
+        genere = findViewById(R.id.genereName);
+        idioma = findViewById(R.id.idioma);
+        edicio= findViewById(R.id.edicio);
+        dataPublicacio = findViewById(R.id.dataPubli);
+
 
         //recollint el intent en format json y transformant-lo a objecte
         String jsonLlibre = getIntent().getStringExtra("llibreJson");
