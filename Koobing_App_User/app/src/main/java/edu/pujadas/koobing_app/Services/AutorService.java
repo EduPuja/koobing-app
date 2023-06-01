480647a611ec567e34ad8611ce8d3f859769019f
Eduard <edpufa@inspalamos.cat>
2023-06-01 18:40:03 +0200

    cambiant a response body perque aixi consturiexo millor el objecte

--- Koobing_App_User/app/src/main/java/edu/pujadas/koobing_app/Services/AutorService.java
+++ Koobing_App_User/app/src/main/java/edu/pujadas/koobing_app/Services/AutorService.java
@@ -1,6 +1,7 @@
 package edu.pujadas.koobing_app.Services;
 
-import edu.pujadas.koobing_app.Models.Autor;
+
+import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Path;
@@ -8,5 +9,5 @@ import retrofit2.http.Path;
 public interface AutorService {
 
     @GET("/author/{id}")
-    Call<Autor> getAutorById(@Path("id") int id);
+    Call<ResponseBody> getAutorById(@Path("id") int id);
 }
