f2c371310078ae0d3c6983d38c08ae556eba7b61
Eduard <edpufa@inspalamos.cat>
2023-06-01 20:29:52 +0200

    arreglant el loader de autor

--- Koobing_App_User/app/src/main/java/edu/pujadas/koobing_app/Loaders/AutorLoader.java
+++ Koobing_App_User/app/src/main/java/edu/pujadas/koobing_app/Loaders/AutorLoader.java
@@ -1,5 +1,7 @@
 package edu.pujadas.koobing_app.Loaders;
 
+import com.google.gson.JsonObject;
+
 import org.json.JSONObject;
 
 import java.sql.Date;
@@ -14,20 +16,21 @@ import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class AutorLoader {
 
 
-    public void getAutorByID(int id, final ApiCallback<Autor> callback)
-    {
-        String url = "http://192.168.0.33:3000/autor/" + id+"/";
+    public void getAutorById(int id, final ApiCallback<Autor> callback) {
+        String url = "http://localhost:3000/author/" + id;
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(url)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
 
-        RetrofitConnection retrofitConnection = new RetrofitConnection(url);
-
-        //creant el servei de post
-        AutorService autorService = retrofitConnection.getRetrofit().create(AutorService.class);
-
-        Call<ResponseBody> call = autorService.getAutorById(id);
+        AutorService service = retrofit.create(AutorService.class);
+        Call<ResponseBody> call = service.getAutorById(id);
 
         call.enqueue(new Callback<ResponseBody>() {
 
@@ -35,29 +38,23 @@ public class AutorLoader {
             public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
                 if(response.isSuccessful())
                 {
-                    String jsonResponse = null;
                     try{
-                        jsonResponse = response.body().string();
+                        String json = response.body().string();
 
-                        if(jsonResponse != null )
-                        {
-                            JSONObject jsonObject = new JSONObject(jsonResponse);
-                            Autor autor = new Autor();
-                            autor.setIdAutor(jsonObject.getInt("id_autor"));
-                            autor.setNomAutor(jsonObject.getString("nom_autor"));
+                        JSONObject jsonObject = new JSONObject(json);
+                        Autor autor = new Autor();
+                        autor.setIdAutor(jsonObject.getInt("id_autor"));
+                        autor.setNomAutor(jsonObject.getString("nom_autor"));
+                        //convertir data naix
+                        String fecha = jsonObject.getString("data_naix");
+                        SimpleDateFormat formatoFecha = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");
+                        formatoFecha.setTimeZone(TimeZone.getTimeZone("UTC"));
+                        java.util.Date utilDate = formatoFecha.parse(fecha);
+                        Date sqlDate = new java.sql.Date(utilDate.getTime());
 
-                            // convertint la data de naiamnet
-                            String fecha = jsonObject.getString("data_naix");
-                            SimpleDateFormat formatoFecha = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");
-                            formatoFecha.setTimeZone(TimeZone.getTimeZone("UTC"));
-                            java.util.Date utilDate = formatoFecha.parse(fecha);
-                            Date sqlDate = new java.sql.Date(utilDate.getTime());
-                            autor.setDataNaixAutor(sqlDate);
+                        autor.setDataNaixAutor(sqlDate);
+                        callback.onSuccess(autor);
 
-                            //envio el callback suscces
-
-                            callback.onSuccess(autor);
-                        }
                     }
                     catch (Exception e)
                     {
@@ -66,16 +63,18 @@ public class AutorLoader {
                 }
                 else
                 {
-                    //en cas d'error envio el codi
+                    // error envio codi error
                     callback.onError(response.code());
                 }
             }
 
             @Override
             public void onFailure(Call<ResponseBody> call, Throwable t) {
-
+                callback.onFailure(t);
             }
         });
+
+
 
     }
 }
