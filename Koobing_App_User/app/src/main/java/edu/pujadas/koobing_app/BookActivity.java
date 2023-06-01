f9abdb1b849061b5cb7b0f599240f3e4b6dcff92
Eduard <edpufa@inspalamos.cat>
2023-06-01 17:54:58 +0200

    no fa peticio

--- Koobing_App_User/app/src/main/java/edu/pujadas/koobing_app/BookActivity.java
+++ Koobing_App_User/app/src/main/java/edu/pujadas/koobing_app/BookActivity.java
@@ -20,6 +20,12 @@ import java.net.Socket;
 import edu.pujadas.koobing_app.Loaders.LlibreLoader;
 import edu.pujadas.koobing_app.Models.Llibre;
 import edu.pujadas.koobing_app.Services.ApiCallback;
+import edu.pujadas.koobing_app.Services.LlibreService;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class BookActivity extends AppCompatActivity {
 
@@ -39,47 +45,47 @@ public class BookActivity extends AppCompatActivity {
         editorial = findViewById(R.id.editorialName);
         genere = findViewById(R.id.genereName);
         idioma = findViewById(R.id.idioma);
-        edicio= findViewById(R.id.edicio);
+        edicio = findViewById(R.id.edicio);
         dataPublicacio = findViewById(R.id.dataPubli);
 
 
         Intent intent = getIntent();
 
-        if(intent.hasExtra("bookGson"))
-        {
+        if (intent.hasExtra("bookGson")) {
             String bookGson = intent.getStringExtra("bookGson");
             Gson gson = new Gson();
             Llibre llibreOnlyIsbn = gson.fromJson(bookGson, Llibre.class);
 
-            LlibreLoader bookLoader = new LlibreLoader();
+            String url = "http://192.168.0.33:3000/book/" + llibreOnlyIsbn.getISBN() + "/";
 
-            bookLoader.findBookByISBN(new ApiCallback<Llibre>() {
+            Retrofit retrofit = new Retrofit.Builder()
+                    .baseUrl(url)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+
+            LlibreService apiService = retrofit.create(LlibreService.class);
+            Call<Llibre> call = apiService.getBookByISBN(llibreOnlyIsbn.getISBN());
+            call.enqueue(new Callback<Llibre>() {
                 @Override
-                public void onSuccess(Llibre data) {
-                    Toast.makeText(getApplicationContext(),"Succes?",Toast.LENGTH_SHORT).show();
+                public void onResponse(Call<Llibre> call, Response<Llibre> response) {
+                    if (response.isSuccessful()) {
+                        Llibre book = response.body();
+
+                        System.out.println("Tota info Llibre: "+book.getAllInfoBook());
+
+
+                    }
+                    else {
+
+                    }
                 }
 
                 @Override
-                public void onError(int statusCode) {
-                    Toast.makeText(getApplicationContext(),"Error? :" +statusCode,Toast.LENGTH_SHORT).show();
-                }
+                public void onFailure(Call<Llibre> call, Throwable t) {
 
-                @Override
-                public void onFailure(Throwable throwable) {
-                    Toast.makeText(getApplicationContext(),"Failure?",Toast.LENGTH_SHORT).show();
                 }
-            }, llibreOnlyIsbn.getISBN());
+            });
 
-            Toast.makeText(this,"ISBN: " +llibreOnlyIsbn.getISBN(),Toast.LENGTH_SHORT).show();
         }
-
-
-
-
-
-
-
-
-
     }
 }
