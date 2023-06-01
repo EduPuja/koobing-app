55a312255e2578bc527130740b5a89ec4411c801
Eduard <edpufa@inspalamos.cat>
2023-06-01 17:12:52 +0200

    afegint el get per isbn

--- Koobing_App_User/app/src/main/java/edu/pujadas/koobing_app/Services/LlibreService.java
+++ Koobing_App_User/app/src/main/java/edu/pujadas/koobing_app/Services/LlibreService.java
@@ -5,9 +5,12 @@ import java.util.List;
 import edu.pujadas.koobing_app.Models.Llibre;
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Path;
 
 public interface LlibreService {
 
     @GET("/books")
     Call<List<Llibre>> getAllBooks();
+    @GET("/books/{isbn}")
+    Call<Llibre> getBookByISBN(@Path("isbn") long isbn);
 }
