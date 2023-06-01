dbfe83c692f1275495decb92bb1a0fc51eabc0eb
Eduard <edpufa@inspalamos.cat>
2023-06-01 20:43:06 +0200

    arreglant tots els errors de idioma loader

--- Koobing_App_User/app/src/main/java/edu/pujadas/koobing_app/Loaders/IdiomaLoader.java
+++ Koobing_App_User/app/src/main/java/edu/pujadas/koobing_app/Loaders/IdiomaLoader.java
@@ -1,5 +1,7 @@
 package edu.pujadas.koobing_app.Loaders;
 
+import org.json.JSONObject;
+
 import edu.pujadas.koobing_app.Models.Idioma;
 import edu.pujadas.koobing_app.Services.ApiCallback;
 import edu.pujadas.koobing_app.Services.IdiomaService;
@@ -12,8 +14,14 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class IdiomaLoader {
 
+
+    /**
+     * Metode que carregar el idioma per el id
+     * @param id id per buscar
+     * @param callback callback de tipus idioma
+     */
     public void getIdiomaById(int id, final ApiCallback<Idioma> callback) {
-        String url = "http://localhost:3000/idioma/" + id;
+        String url = "http://192.168.0.33:3000/idioma/" + id+"/";
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl(url)
                 .addConverterFactory(GsonConverterFactory.create())
@@ -25,12 +33,33 @@ public class IdiomaLoader {
 
             @Override
             public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
-                
+                if(response.isSuccessful())
+                {
+                    try
+                    {
+                        String json= response.body().string();
+                        JSONObject jsonObject = new JSONObject(json);
+
+                        Idioma idioma =new Idioma();
+                        idioma.setIdIdioma(jsonObject.getInt("id_idioma"));
+                        idioma.setNomIdioma(jsonObject.getString("nom_idioma"));
+
+                        callback.onSuccess(idioma);
+                    }
+                    catch (Exception e)
+                    {
+                        e.printStackTrace();
+                    }
+                }
+
+                else {
+                    callback.onError(response.code());
+                }
             }
 
             @Override
             public void onFailure(Call<ResponseBody> call, Throwable t) {
-
+                callback.onFailure(t);
             }
         });
     }
