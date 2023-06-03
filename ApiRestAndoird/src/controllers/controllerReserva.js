3d096f77eb4c44901f69c181fd5ab15e653e4c78
Eduard <edpufa@inspalamos.cat>
2023-06-03 21:52:38 +0200

    ja fa la reserva

--- ApiRestAndoird/src/controllers/controllerReserva.js
+++ ApiRestAndoird/src/controllers/controllerReserva.js
@@ -7,13 +7,16 @@ function setReserva(req, res) {
   const reserva = req.body;
 
   // Obtener los valores de la reserva
-  const dataFI = reserva.dataFI;
+  const dataInici = new Date();
+  //convertirn la data de finalizacio en un tipus date per que es pugui insertar correctament
+  const dataFIString = reserva.dataFI;
+  const dataFI = new Date(Date.parse(dataFIString));
+  
   const estat = reserva.estat;
   const idReserva = reserva.idReserva;
   const llibreISBN = reserva.llibre.ISBN;
   const treballadorId = reserva.treballador.id;
   const usuariId = reserva.usuari.id;
-  
 
   // Mostrar los valores específicos de la reserva
   console.log("Data FI:", dataFI);
@@ -23,12 +26,15 @@ function setReserva(req, res) {
   console.log("ID del Trabajador:", treballadorId);
   console.log("ID del Usuario:", usuariId);
 
-  //Consulta SQL
-  const query = `INSERT INTO reserva (id_prestec, ISBN, id_usuari, id_treballador, data_inici, data_fi, id_estat)  VALUES (?, ?, ?, ?, ?, ?, ?)`;
+  // Consulta SQL con placeholders
+  const query = `INSERT INTO reserva (id_prestec, ISBN, id_usuari, id_treballador, data_inici, data_fi, id_estat) 
+                 VALUES (?, ?, ?, ?, ?, ?, ?)`;
+  
+  // Valores a insertar
+  const values = [idReserva, llibreISBN, usuariId, treballadorId, dataInici, dataFI, estat];
 
-  const valors = [dataFI, estat, idReserva, llibreISBN, treballadorId, usuariId];
-
-  connection.query(query,[dataFI, estat, idReserva, llibreISBN, treballadorId, usuariId],(error,result) =>{
+  // Ejecutar la consulta preparada
+  connection.query(query, values, (error, result) => {
     if (error) {
       console.error("Error al insertar reserva:", error.message);
       res.status(500).json({ message: "Error al insertar reserva" });
@@ -36,11 +42,10 @@ function setReserva(req, res) {
       console.log("Reserva insertada correctamente");
       res.status(200).json({ message: "Reserva creada correctamente" });
     }
-  })
+  });
 
-  // Enviar una respuesta al cliente
-  res.status(200).json({ message: "Reserva creada correctamente" });
 }
+
 
 
 function getLlibresReservats(req,res)
