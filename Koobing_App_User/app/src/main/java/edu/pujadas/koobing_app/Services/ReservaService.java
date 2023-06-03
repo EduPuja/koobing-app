3ef8ccd4e9a0d321f33ed0adb54c9f5acbd1fb8b
Eduard <edpufa@inspalamos.cat>
2023-06-03 12:13:00 +0200

    arreglant el reservaServce

--- Koobing_App_User/app/src/main/java/edu/pujadas/koobing_app/Services/ReservaService.java
+++ Koobing_App_User/app/src/main/java/edu/pujadas/koobing_app/Services/ReservaService.java
@@ -1,18 +1,20 @@
 package edu.pujadas.koobing_app.Services;
 
 import edu.pujadas.koobing_app.Models.Reserva;
+import edu.pujadas.koobing_app.Models.Usuari;
 import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.GET;
 import retrofit2.http.POST;
+import retrofit2.http.Path;
 
 public interface ReservaService {
 
     @POST("/reservarLlibre")
     Call<Void> hacerReserva(@Body Reserva reserva);
 
-    @GET("/llibresReservats")
-    Call<ResponseBody> obtenirLlibresReservats(@Body Reserva reserva);
+    @GET("/llibresReservats/{id_usuari}")
+    Call<ResponseBody> obtenirLlibresReservats(@Path("id_usuari") int id_usuari);
 
 }
