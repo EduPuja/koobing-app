603310ce3bf3a45dcfb0e8b5ef9f77d9fe4864c4
Eduard <edpufa@inspalamos.cat>
2023-06-03 15:08:36 +0200

    provant si de veritat retorna la info correctament

--- Koobing_App_User/app/src/main/java/edu/pujadas/koobing_app/Loaders/EditorialLoader.java
+++ Koobing_App_User/app/src/main/java/edu/pujadas/koobing_app/Loaders/EditorialLoader.java
@@ -58,4 +58,46 @@ public class EditorialLoader {
             }
         });
     }*/
+
+    public void getEditorialById(int id, final ApiCallback<Editorial> callback) {
+        String url = "http://192.168.0.33:3000/editor/" + id+"/";
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(url)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        EditorialService service = retrofit.create(EditorialService.class);
+        Call<Editorial> call = service.getEditorialById(id);
+        call.enqueue(new Callback<Editorial>() {
+
+            @Override
+            public void onResponse(Call<Editorial> call, Response<Editorial> response) {
+                if(response.isSuccessful())
+                {
+
+                    try
+                    {
+                        Editorial editorial = response.body();
+
+
+                        callback.onSuccess(editorial);
+                    }
+                    catch (Exception e)
+                    {
+                        e.printStackTrace();
+                    }
+                }
+                else
+                {
+                    callback.onError(response.code());
+                }
+            }
+
+            @Override
+            public void onFailure(Call<Editorial> call, Throwable t) {
+                callback.onFailure(t);
+            }
+        });
+    }
+
 }
