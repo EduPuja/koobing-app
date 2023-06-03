300616ef51d04f3dbf5d34eaa665ed6d6c7f68be
Eduard <edpufa@inspalamos.cat>
2023-06-03 15:39:13 +0200

    afegint el idioma & genere  i la data de publicacio

--- Koobing_App_User/app/src/main/java/edu/pujadas/koobing_app/Deserializer/LlibreDeserializer.java
+++ Koobing_App_User/app/src/main/java/edu/pujadas/koobing_app/Deserializer/LlibreDeserializer.java
@@ -8,11 +8,16 @@ import com.google.gson.JsonParseException;
 
 import java.io.IOException;
 import java.lang.reflect.Type;
+import java.sql.Date;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Locale;
+import java.util.TimeZone;
 
 import edu.pujadas.koobing_app.Loaders.AutorLoader;
 import edu.pujadas.koobing_app.Loaders.EditorialLoader;
+import edu.pujadas.koobing_app.Loaders.GenereLoader;
+import edu.pujadas.koobing_app.Loaders.IdiomaLoader;
 import edu.pujadas.koobing_app.Models.Autor;
 import edu.pujadas.koobing_app.Models.Editorial;
 import edu.pujadas.koobing_app.Models.Genere;
@@ -54,7 +59,7 @@ public class LlibreDeserializer implements JsonDeserializer<Llibre> {
         llibre.setISBN(isbn);
         llibre.setTitol(titol);
         llibre.setVersio(versio);
-       // llibre.setDataPubli(dataPubli);
+       llibre.setDataPubli(parseFechaPublicacion(dataPubliStr));
         llibre.setStock(stock);
 
         // -- Obtenit els objectes dels loader
@@ -98,8 +103,76 @@ public class LlibreDeserializer implements JsonDeserializer<Llibre> {
             }
         });
 
+        //idioma
+        IdiomaLoader idiomaLoader = new IdiomaLoader();
+        idiomaLoader.getIdiomaById(idIdioma, new ApiCallback<Idioma>() {
+            @Override
+            public void onSuccess(Idioma idioma) {
+                llibre.setIdioma(idioma);
+            }
+
+            @Override
+            public void onError(int statusCode) {
+                // Manejar el error si la obtención del Idioma falla
+                System.out.println("Error BOOK_DESERIALIZE Idioma: " + statusCode);
+            }
+
+            @Override
+            public void onFailure(Throwable throwable) {
+                // Manejar la falla si ocurre algún error de comunicación
+                System.out.println("Failure BOOK_DESERIALIZE Idioma: " + throwable.getMessage());
+            }
+        });
+
+
+        //genere
+        GenereLoader genereLoader = new GenereLoader();
+        genereLoader.getGenereById(idGenere, new ApiCallback<Genere>() {
+            @Override
+            public void onSuccess(Genere genere) {
+                llibre.setGenere(genere);
+            }
+
+            @Override
+            public void onError(int statusCode) {
+                // Manejar el error si la obtención del Genere falla
+                System.out.println("Error BOOK_DESERIALIZE genere: " + statusCode);
+
+            }
+
+            @Override
+            public void onFailure(Throwable throwable) {
+                System.out.println("Failure BOOK_DESERIALIZE genere: " + throwable.getMessage());
+            }
+        });
+
 
         return llibre;
+    }
+
+    /**
+     * Metode per transformar la data de publicacio del llibre en un SQL DATE
+     * @param fechaString data en formt string
+     * @return java.sql.Date / null
+     */
+    private Date parseFechaPublicacion(String fechaString) {
+        try {
+            // Formato de fecha en el JSON: "2023-05-10T22:00:00.000Z"
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");
+            sdf.setTimeZone(TimeZone.getTimeZone("UTC"));
+
+            // Parsear la fecha de String a java.util.Date
+            java.util.Date utilDate = sdf.parse(fechaString);
+
+            // Convertir java.util.Date a java.sql.Date
+            java.sql.Date sqlDate = new java.sql.Date(utilDate.getTime());
+
+            return sqlDate;
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        return null; // Devolver null en caso de error
     }
 
 
