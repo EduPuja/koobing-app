41c0fe509c67606fc961b5f0de4d262464782b36
Eduard <edpufa@inspalamos.cat>
2023-06-03 14:58:44 +0200

    cambiant de tipus objecte

--- Koobing_App_User/app/src/main/java/edu/pujadas/koobing_app/Services/EditorialService.java
+++ Koobing_App_User/app/src/main/java/edu/pujadas/koobing_app/Services/EditorialService.java
@@ -1,5 +1,6 @@
 package edu.pujadas.koobing_app.Services;
 
+import edu.pujadas.koobing_app.Models.Editorial;
 import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.http.GET;
@@ -8,5 +9,5 @@ import retrofit2.http.Path;
 public interface EditorialService {
 
     @GET("/editor/{id}")
-    Call<ResponseBody> getEditorialById(@Path("id") int id);
+    Call<Editorial> getEditorialById(@Path("id") int id);
 }
