5c987c41e1928df029c3957ddf4debae128d8c25
Eduard <edpufa@inspalamos.cat>
2023-06-03 14:54:40 +0200

    arreglant el deserializer

--- Koobing_App_User/app/src/main/java/edu/pujadas/koobing_app/Deserializer/LlibreDeserializer.java
+++ Koobing_App_User/app/src/main/java/edu/pujadas/koobing_app/Deserializer/LlibreDeserializer.java
@@ -15,11 +15,27 @@ import edu.pujadas.koobing_app.Models.Editorial;
 import edu.pujadas.koobing_app.Models.Genere;
 import edu.pujadas.koobing_app.Models.Idioma;
 import edu.pujadas.koobing_app.Models.Llibre;
+import edu.pujadas.koobing_app.Services.AutorService;
+import edu.pujadas.koobing_app.Services.EditorialService;
+import edu.pujadas.koobing_app.Services.GenereService;
+import edu.pujadas.koobing_app.Services.IdiomaService;
 
 public class LlibreDeserializer implements JsonDeserializer<Llibre> {
 
+    private AutorService autorService;
+    private EditorialService editorialService;
+    private IdiomaService idiomaService;
+    private GenereService genereService;
+
+    public LlibreDeserializer(AutorService autorService, EditorialService editorialService, IdiomaService idiomaService, GenereService genereService) {
+        this.autorService = autorService;
+        this.editorialService = editorialService;
+        this.idiomaService = idiomaService;
+        this.genereService = genereService;
+    }
+
     @Override
-    public Llibre deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws Ex {
+    public Llibre deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException  {
         JsonObject jsonObject = json.getAsJsonObject();
 
         // Obtener los campos del JSON
