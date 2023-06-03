5362772ee4bbdffa7219513161578835253c503f
Eduard <edpufa@inspalamos.cat>
2023-06-03 11:53:49 +0200

    creant un loader per carregar els gets de les reserves

--- Koobing_App_User/app/src/main/java/edu/pujadas/koobing_app/Services/ReservaService.java
+++ Koobing_App_User/app/src/main/java/edu/pujadas/koobing_app/Services/ReservaService.java
@@ -1,12 +1,18 @@
 package edu.pujadas.koobing_app.Services;
 
 import edu.pujadas.koobing_app.Models.Reserva;
+import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.GET;
 import retrofit2.http.POST;
 
 public interface ReservaService {
 
     @POST("/reservarLlibre")
     Call<Void> hacerReserva(@Body Reserva reserva);
+
+    @GET("/llibresReservats")
+    Call<ResponseBody> obtenirLlibresReservats(@Body Reserva reserva);
+
 }
