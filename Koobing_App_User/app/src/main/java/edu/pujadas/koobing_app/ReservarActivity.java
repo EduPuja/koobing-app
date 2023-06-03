adb2fba185a00787034e92891b83a3c36b06b48e
Eduard <edpufa@inspalamos.cat>
2023-06-03 19:42:24 +0200

    creant la actiivty de reservar

---
+++ Koobing_App_User/app/src/main/java/edu/pujadas/koobing_app/ReservarActivity.java
@@ -0,0 +1,14 @@
+package edu.pujadas.koobing_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ReservarActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.reservar_activity);
+    }
+}
