383f6010fc74be153c50a0c3fb9ae5888b5ea181
Eduard <edpufa@inspalamos.cat>
2023-06-04 11:52:41 +0200

    millorant el codi

--- Koobing_App_User/app/src/main/java/edu/pujadas/koobing_app/ProfileActivit.java
+++ Koobing_App_User/app/src/main/java/edu/pujadas/koobing_app/ProfileActivit.java
@@ -5,6 +5,8 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.TextView;
@@ -16,10 +18,13 @@ import edu.pujadas.koobing_app.Adapters.LlibreAdapter;
 import edu.pujadas.koobing_app.Loaders.LlibreLoader;
 import edu.pujadas.koobing_app.Models.Llibre;
 import edu.pujadas.koobing_app.Models.Usuari;
+import edu.pujadas.koobing_app.Utilites.RetrofitConnection;
 import edu.pujadas.koobing_app.Utilites.UsuarioSingleton;
 
 public class ProfileActivit extends AppCompatActivity {
 
+
+    String baseUrl = "http://192.168.0.33:3000/";
     TextView nom,cognom,email,dni;
     LlibreLoader llibreLoader;
     LlibreAdapter bookAdapter;
@@ -33,13 +38,49 @@ public class ProfileActivit extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.profile_activity);
-
+        //find by id
         nom = findViewById(R.id.userName);
         cognom = findViewById(R.id.userSurname);
         email = findViewById(R.id.userEmail);
         dni = findViewById(R.id.userDni);
         spinner = findViewById(R.id.spinner);
+        // load info user
+        loadInfoUser();
+        // start the spinner
+        startSpinner();
 
+        listLlibres = new ArrayList<Llibre>();
+
+        //initialiazing the recycler view
+        /*recyclerView = findViewById(R.id.booksRecycler);
+        layoutManager = new LinearLayoutManager(this);
+        layoutManager.setOrientation(RecyclerView.VERTICAL);
+        recyclerView.setLayoutManager(layoutManager);
+        bookAdapter = new LlibreAdapter(listLlibres);
+        recyclerView.setAdapter(bookAdapter);*/
+
+    }
+
+
+    /**
+     * Metode per carregar la informació del usuari
+     */
+    public void loadInfoUser()
+    {
+        // obntenint la info del usuari per posar-ho per pentalla
+        Usuari usuari =UsuarioSingleton.getInstance().getUsuario();
+
+        nom.setText(usuari.getNom());
+        cognom.setText(usuari.getCognom());
+        email.setText(usuari.getEmail());
+        dni.setText(usuari.getDni());
+    }
+
+    /**
+     * Metode que inicialitza el spinner
+     */
+    public void startSpinner()
+    {
         List<String> opciones = new ArrayList<>();
         opciones.add("1.Reservats");
         opciones.add("2.Cancelats");
@@ -51,22 +92,43 @@ public class ProfileActivit extends AppCompatActivity {
         spinner.setAdapter(adapterSipnner);
 
 
-        listLlibres = new ArrayList<Llibre>();
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
 
-        //initialiazing the recycler view
-        recyclerView = findViewById(R.id.booksRecycler);
-        layoutManager = new LinearLayoutManager(this);
-        layoutManager.setOrientation(RecyclerView.VERTICAL);
-        recyclerView.setLayoutManager(layoutManager);
-        bookAdapter = new LlibreAdapter(listLlibres);
-        recyclerView.setAdapter(bookAdapter);
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String opcionSelected = opciones.get(position);
+            }
 
-        Usuari usuari =UsuarioSingleton.getInstance().getUsuario();
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
 
-        nom.setText(usuari.getNom());
-        cognom.setText(usuari.getCognom());
-        email.setText(usuari.getEmail());
-        dni.setText(usuari.getDni());
+            }
+        });
+    }
+
+
+    public void sendRequest(String option)
+    {
+        if(option.equals("1.Reservats")) {
+
+            // fent la peticio per els reservats
+            RetrofitConnection retrofitConnection = new RetrofitConnection(baseUrl);
+
+
+
+        }
+        else if(option.equals("2.Cancelats"))
+        {
+
+        }
+        else if(option.equals("3.Tornats"))
+        {
+
+        }
+        else if (option.equals("4.En Prèstec"))
+        {
+
+        }
     }
 
 }
