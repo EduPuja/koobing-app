1b154d0cc15e7b6c3ee8f73271cfed561e532c91
Eduard <edpufa@inspalamos.cat>
2023-06-04 14:16:32 +0200

    posant el reservaAdapter

--- Koobing_App_User/app/src/main/java/edu/pujadas/koobing_app/Adapters/ReservaAdapter.java
+++ Koobing_App_User/app/src/main/java/edu/pujadas/koobing_app/Adapters/ReservaAdapter.java
@@ -1,31 +1,59 @@
 package edu.pujadas.koobing_app.Adapters;
 
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.List;
+
+import edu.pujadas.koobing_app.Models.Reserva;
+import edu.pujadas.koobing_app.R;
+
 public class ReservaAdapter  extends RecyclerView.Adapter<ReservaAdapter.ResevaViewHolder>{
+
+    List<Reserva> listReserva;
+
+    public ReservaAdapter(List<Reserva> listReserva) {
+        this.listReserva = listReserva;
+    }
+
     @NonNull
     @Override
     public ReservaAdapter.ResevaViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View vista = LayoutInflater.from(parent.getContext()).inflate(R.layout.llibre_items ,parent,false);
+
+        return new ReservaAdapter.ResevaViewHolder(vista);
     }
 
     @Override
     public void onBindViewHolder(@NonNull ReservaAdapter.ResevaViewHolder holder, int position) {
-
+        String titleLlibre = listReserva.get(position).getLlibre().getTitol();
+        holder.setData(titleLlibre);
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return listReserva.size();
     }
 
     public static class ResevaViewHolder extends RecyclerView.ViewHolder {
+
+        ImageView portada;
+        TextView titol;
         public ResevaViewHolder(@NonNull View itemView) {
             super(itemView);
+            portada = itemView.findViewById(R.id.portada);
+            titol = itemView.findViewById(R.id.bookTitle);
+
+        }
+
+        public void setData(String title) {
+            this.titol.setText(title);
         }
     }
 }
--- Koobing_App_User/app/src/main/java/edu/pujadas/koobing_app/ProfileActivit.java
+++ Koobing_App_User/app/src/main/java/edu/pujadas/koobing_app/ProfileActivit.java
@@ -19,6 +19,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import edu.pujadas.koobing_app.Adapters.LlibreAdapter;
+import edu.pujadas.koobing_app.Adapters.ReservaAdapter;
 import edu.pujadas.koobing_app.Loaders.LlibreLoader;
 import edu.pujadas.koobing_app.Models.Llibre;
 import edu.pujadas.koobing_app.Models.Reserva;
